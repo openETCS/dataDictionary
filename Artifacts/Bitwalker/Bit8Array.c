b42e70d7072173e3ccf40d7e2f21a015ccd004b0
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-08-21 13:30:19 +0200

    fixes #5

--- Artifacts/Bitwalker/Bit8Array.c
+++ Artifacts/Bitwalker/Bit8Array.c
@@ -20,28 +20,32 @@ void PokeBit8Array(uint8_t* Bitstream,
 
   Bitstream[i] = PokeBit8(Bitstream[i], k, flag);
 
-  /*@
-    assert \forall integer j; 0 <= j < k ==>
-      (LeftBit8(Bitstream[left_index/8], j) <==>
-        \at(LeftBit8(Bitstream[left_index/8], j), Pre));
-  */
+  // The following two assertions claim that in byte with index "left_index/8"
+  // the bits with indices different from "k" do not change
+    /*@
+      assert \forall integer j; 0 <= j < k ==>
+        (LeftBit8(Bitstream[left_index/8], j) <==>
+          \at(LeftBit8(Bitstream[left_index/8], j), Pre));
+    */
 
-  /*@
-    assert \forall integer j; k < j < 8 ==>
-      (LeftBit8(Bitstream[left_index/8], j) <==>
-        \at(LeftBit8(Bitstream[left_index/8], j), Pre));
-  */
+    /*@
+      assert \forall integer j; k < j < 8 ==>
+        (LeftBit8(Bitstream[left_index/8], j) <==>
+          \at(LeftBit8(Bitstream[left_index/8], j), Pre));
+    */
 
-  /*@
-    assert \forall integer a, j;
-      ((0 <= a < left_index/8) && (0 <= j < 8)) ==>
-        (LeftBit8(Bitstream[a], j) <==> \at(LeftBit8(Bitstream[a], j), Pre));
-  */
+  // The following two assertions claim that in every byte
+  // with an index that is different from "left_index/8" no bit is changed.
+    /*@
+      assert \forall integer l, j;
+        ((0 <= l < left_index/8) && (0 <= j < 8)) ==>
+          (LeftBit8(Bitstream[l], j) <==> \at(LeftBit8(Bitstream[l], j), Pre));
+    */
 
-  /*@
-    assert \forall integer a, j;
-      ((left_index/8 < a < BitstreamSize) && (0 <= j < 8)) ==>
-        (LeftBit8(Bitstream[a], j) <==> \at(LeftBit8(Bitstream[a], j), Pre));
-  */
+    /*@
+      assert \forall integer l, j;
+        ((left_index/8 < l < BitstreamSize) && (0 <= j < 8)) ==>
+          (LeftBit8(Bitstream[l], j) <==> \at(LeftBit8(Bitstream[l], j), Pre));
+    */
 }
 
