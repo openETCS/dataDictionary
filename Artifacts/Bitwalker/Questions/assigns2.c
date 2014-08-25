6addbfdbba48290779ac3e7fd2dddc838579dc51
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-08-25 15:42:02 +0200

    rewrite to make it more similar to  sqrt

--- Artifacts/Bitwalker/Questions/assigns2.c
+++ Artifacts/Bitwalker/Questions/assigns2.c
@@ -6,14 +6,14 @@ int global_state;
 
    behavior non_negative:
      assumes n >= 0;
-     assigns global_state;
-     ensures global_state == 0;
+     assigns \nothing;
      ensures \result == 1;
 
    behavior negative:
      assumes n < 0;
-     assigns \nothing;
-     ensures \result == -1;
+     assigns global_state;
+     ensures \result == 0;
+     ensures global_state == -1;
 
   complete behaviors;
   disjoint behaviors;
@@ -22,27 +22,29 @@ int f(int n)
 {
   if (n >= 0)
   {
-    global_state = 0;
     return 1;
   }
   else
   {
-    return -1;
+    global_state = -1;
+    return 0;
   }
 }
 
 
 /*@
+   assigns global_state;
+
    behavior non_negative:
      assumes n >= 0;
      assigns global_state;
-     ensures global_state == 0;
      ensures \result == 1;
 
    behavior negative:
      assumes n < 0;
      assigns global_state;
-     ensures \result == -1;
+     ensures global_state == -1;
+     ensures \result == 0;
 
   complete behaviors;
   disjoint behaviors;
