768bc93f0719febeb191ff4f6d35012e0031572e
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-11-11 13:46:34 +0100

    turn elementary bit functions into inline functions

--- Artifacts/Bitwalker/Bit64.h
+++ Artifacts/Bitwalker/Bit64.h
@@ -11,8 +11,14 @@
 
    ensures (\result != 0) <==> LeftBit64(value, left_index);
 */
-int PeekBit64(uint64_t value, uint32_t left_index);
+static inline int PeekBit64(uint64_t value, uint32_t left_index)
+{
+  uint64_t mask = ((uint64_t) 1) << (63u - left_index);
+  uint64_t flag = value & mask;
+  //@ assert (flag != 0) <==> LeftBit64(value, left_index);
 
+  return flag != 0u;
+}
 
 
 /*@
@@ -28,7 +34,12 @@ int PeekBit64(uint64_t value, uint32_t left_index);
     ensures \forall integer i; (left_index < i < 64) ==>
                 (LeftBit64(\result, i) <==> LeftBit64(value, i));
 */
-uint64_t PokeBit64(uint64_t value, uint32_t left_index, int flag);
+static inline uint64_t PokeBit64(uint64_t value, uint32_t left_index, int flag)
+{
+  uint64_t mask = ((uint64_t) 1u) << (63 - left_index);
+
+  return (flag == 0) ? (value & ~mask) : (value | mask);
+}
 
 #endif
 
--- Artifacts/Bitwalker/Bit8.h
+++ Artifacts/Bitwalker/Bit8.h
@@ -12,7 +12,13 @@
 
     ensures (\result != 0) <==> LeftBit8(byte, left_index);
 */
-int PeekBit8(uint8_t byte, uint32_t left_index);
+static inline int PeekBit8(uint8_t byte, uint32_t left_index)
+{
+  uint8_t mask = ((uint8_t) 1) << (7u - left_index);
+  uint8_t flag = byte & mask;
+
+  return flag != 0;
+}
 
 
 /*@
@@ -28,7 +34,12 @@ int PeekBit8(uint8_t byte, uint32_t left_index);
     ensures \forall integer i; (left_index < i < 8) ==>
               (LeftBit8(\result, i) <==> LeftBit8(byte, i));
 */
-uint8_t PokeBit8(uint8_t byte, uint32_t left_index, int flag);
+static inline uint8_t PokeBit8(uint8_t byte, uint32_t left_index, int flag)
+{
+  uint8_t mask = ((uint8_t) 1) << (7u - left_index);
+
+  return (flag == 0) ? (byte & ~mask) : (byte | mask);
+}
 
 #endif
 
