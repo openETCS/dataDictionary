f04386b11db4aa1954cee9190f4d842f273216ba
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-11-12 10:41:41 +0100

    fix names in PokeThenPeek

--- Artifacts/Bitwalker/PokeThenPeek.c
+++ Artifacts/Bitwalker/PokeThenPeek.c
@@ -3,26 +3,26 @@
 #include "Bitwalker_Peek.h"
 #include "Bitwalker_Poke.h"
 
-uint64_t PokeThenPeek(uint32_t  Start,
-                      uint32_t  Length,
-                      uint8_t*  Bitstream,
-                      uint32_t  BitstreamSize,
-                      uint64_t  Value)
+uint64_t PokeThenPeek(uint32_t  start,
+                      uint32_t  length,
+                      uint8_t*  addr,
+                      uint32_t  size,
+                      uint64_t  value)
 {
-  //@ assert \forall integer i; 0 <= i < 64-Length ==> !LeftBit64(Value, i);
+  //@ assert \forall integer i; 0 <= i < 64-length ==> !LeftBit64(value, i);
   int      poke_result = 0;
   uint64_t peek_result = 0u;
 
-  poke_result =  Bitwalker_Poke(Start, Length, Bitstream, BitstreamSize, Value);
+  poke_result =  Bitwalker_Poke(start, length, addr, size, value);
   //@ assert  poke_result == 0;
 
-  peek_result = Bitwalker_Peek(Start, Length, Bitstream, BitstreamSize);
+  peek_result = Bitwalker_Peek(start, length, addr, size);
 
-  /*@ assert \forall integer i; 64-Length <= i < 64 ==>
-             (LeftBit64(peek_result, i) <==> LeftBit64(Value, i)); */
+  /*@ assert \forall integer i; 64-length <= i < 64 ==>
+             (LeftBit64(peek_result, i) <==> LeftBit64(value, i)); */
 
   /*@ assert  \forall integer i; 0 <= i < 64 ==>
-             (LeftBit64(peek_result, i) <==> LeftBit64(Value, i)); */
+             (LeftBit64(peek_result, i) <==> LeftBit64(value, i)); */
 
   return peek_result;
 }
--- Artifacts/Bitwalker/PokeThenPeek.h
+++ Artifacts/Bitwalker/PokeThenPeek.h
@@ -5,21 +5,21 @@
 #include "BitsFramaC.h"
 
 /*@
-    requires \valid(Bitstream + (0..BitstreamSize-1));
-    requires 8 * BitstreamSize <= UINT_MAX;
-    requires 0 <= Length < 64;
-    requires Start + Length <= 8 * BitstreamSize;
-    requires Value < (1 << Length);
+    requires \valid(addr + (0..size-1));
+    requires 8 * size <= UINT_MAX;
+    requires 0 <= length < 64;
+    requires start + length <= 8 * size;
+    requires value < (1 << length);
 
-    assigns Bitstream[0..BitstreamSize-1];
+    assigns addr[0..size-1];
 
-    ensures \result == Value;
+    ensures \result == value;
 */
-uint64_t PokeThenPeek(uint32_t  Start,
-                      uint32_t  Length,
-                      uint8_t*  Bitstream,
-                      uint32_t  BitstreamSize,
-                      uint64_t  Value);
+uint64_t PokeThenPeek(uint32_t  start,
+                      uint32_t  length,
+                      uint8_t*  addr,
+                      uint32_t  size,
+                      uint64_t  value);
 
 #endif
 
