f6e94f6fa2562519ca2f02d48787a158e4ac6d7f
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-11-12 11:33:35 +0100

    provide first implementation of ReadInt (not yet verified)

--- Artifacts/Bitwalker/Bitstream_ReadInt32.c
+++ Artifacts/Bitwalker/Bitstream_ReadInt32.c
@@ -7,6 +7,6 @@ int32_t Bitstream_ReadInt32(Bitstream* stream, uint32_t length)
   // plausibility check is done when reading the bits
   uint64_t a = Bitstream_Read(stream, length);
 
-  return int32_t(a);
+  return (int32_t) a;
 }
 
