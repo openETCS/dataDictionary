612df47a0703a746d70cc76e999e4fded86fca08
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-01 14:29:07 +0100

    add property names for Bit64 functions

--- Artifacts/Bitwalker/Bit64.h
+++ Artifacts/Bitwalker/Bit64.h
@@ -5,24 +5,24 @@
 #include "FramaCBits.h"
 
 /*@
-   requires left < 64;
+   requires pre: left < 64;
 
    assigns \nothing;
 
-   ensures \result != 0 <==> LeftBit64(value, left);
+   ensures set_bit: \result != 0 <==> LeftBit64(value, left);
 */
 int PeekBit64(uint64_t value, uint32_t left);
 
 
 /*@
-    requires left < 64;
+    requires pre: left < 64;
 
     assigns \nothing;
 
-    ensures \forall integer i; (0 <= i < 64  &&  i != left) ==>
+    ensures unchanged_bits: \forall integer i; (0 <= i < 64  &&  i != left) ==>
                 (LeftBit64(\result, i) <==> LeftBit64(value, i));
 
-    ensures flag != 0  <==>  LeftBit64(\result, left);
+    ensures set_bit:  flag != 0  <==>  LeftBit64(\result, left);
 */
 uint64_t PokeBit64(uint64_t value, uint32_t left, int flag);
 
