c27047f198b048aa3e831a0599237b4fe4e8d640
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-04 11:16:31 +0100

    allow to use 64-bit values and adapt first loop invariant

--- Artifacts/Bitwalker/Bitwalker_Poke.c
+++ Artifacts/Bitwalker/Bitwalker_Poke.c
@@ -13,15 +13,18 @@ int Bitwalker_Poke(uint32_t  start,
     return -1;	// error: invalid_bit_sequence
   }
 
-  // compute pow2(length)
-  const uint64_t MaxValue = ((uint64_t) 1) << length;
-  //@ assert MaxValue == 1 << length;
-
-  if (value >= MaxValue)
+  if (length < 64)
   {
-    return -2;  // error: value_too_big
+      // compute pow2(length)
+      const uint64_t MaxValue = ((uint64_t) 1) << length;
+      //@ assert MaxValue == 1 << length;
+    
+      if (value >= MaxValue)
+      {
+        return -2;  // error: value_too_big
+      }
   }
-
+  
   /*@
     loop invariant 0 <= i <= length;
     loop invariant \forall integer k; 0 <= k < start ==>
