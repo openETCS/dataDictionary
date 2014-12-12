789e3d28dd69ac3e94c3b29bc306f8190fdf5600
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-12 16:17:51 +0100

    add property labels and adapt proofs

--- Artifacts/Bitwalker/PokeThenPeek.c
+++ Artifacts/Bitwalker/PokeThenPeek.c
@@ -5,12 +5,12 @@
 
 uint64_t PokeThenPeek(uint8_t* addr, uint32_t size, uint32_t start, uint32_t length, uint64_t value)
 {
-  //@ assert \forall integer i; 0 <= i < 64-length ==> !LeftBit64(value, i);
+  //@ assert bits_covered: \forall integer i; 0 <= i < 64-length ==> !LeftBit64(value, i);
   int      poke_result = 0;
   uint64_t peek_result = 0u;
 
   poke_result =  Bitwalker_Poke(addr, size, start, length, value);
-  //@ assert  poke_result == 0;
+  //@ assert result_zero:  poke_result == 0;
 
   peek_result = Bitwalker_Peek(addr, size, start, length);
 
