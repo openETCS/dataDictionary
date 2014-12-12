3b65b913a1331ea2be40d2b2e19b72312287a611
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-12 20:06:33 +0100

    add another property label and adapt proof

--- Artifacts/Bitwalker/Bitstream_Write.c
+++ Artifacts/Bitwalker/Bitstream_Write.c
@@ -6,7 +6,7 @@ int Bitstream_Write(Bitstream* stream, uint32_t length, uint64_t value)
 {
   int retval = Bitwalker_Poke(stream->addr, stream->size, stream->bitpos, length, value);
 
-  //@ assert stream->bitpos == \at(stream->bitpos, Pre);
+  //@ assert bitpos_unchanged: stream->bitpos == \at(stream->bitpos, Pre);
   stream->bitpos += length;
 
   return retval;
