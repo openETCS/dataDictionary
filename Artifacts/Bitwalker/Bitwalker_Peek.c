e123661d652f1a2b14a03b9dec4df9295344abc6
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-13 13:34:07 +0100

    adapt proof of rewritten property "copied"

--- Artifacts/Bitwalker/Bitwalker_Peek.c
+++ Artifacts/Bitwalker/Bitwalker_Peek.c
@@ -14,11 +14,10 @@ uint64_t Bitwalker_Peek(uint8_t*  addr, uint32_t  size, uint32_t  start, uint32_
   /*@
     loop invariant index:  0 <= i <= length;
 
-    loop invariant copied: \forall integer k; 0 <= k < i ==>
-                    (LeftBit8Array(addr, start+k) <==> LeftBit64(retval, 64-length+k));
+    loop invariant copied: \forall integer k; start <= k < start + i ==>
+                    (LeftBit8Array(addr, k) <==> LeftBit64(retval, 64 - length + k - start));
 
     loop invariant not_set: UpperBitsNotSet64(retval, length);
-    //loop invariant not_set: \forall integer k; length <= k < 64  ==> !BitTest(retval, k);
 
     loop assigns i, retval;
     loop variant length - i;
--- Artifacts/Bitwalker/Bitwalker_Peek.h
+++ Artifacts/Bitwalker/Bitwalker_Peek.h
@@ -21,11 +21,10 @@
     assumes start + length <= 8 * size;
     assigns \nothing;
 
-    ensures copied:  \forall integer i; 0 <= i < length ==>
-              (LeftBit8Array(addr, start+i) <==> LeftBit64(\result, 64-length + i));
+    ensures copied:  \forall integer i; start <= i < start+length ==>
+              (LeftBit8Array(addr, i) <==> LeftBit64(\result, 64-length + i - start));
 
     ensures not_set: UpperBitsNotSet64(\result, length);
-    //ensures not_set: \forall integer i; length <= i < 64  ==> !BitTest(\result, i);
 
     ensures valid_result: \result < (1 << length);
 
