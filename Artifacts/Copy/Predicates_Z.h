2459c85c1773b823795be5f58b8bb7f7cc935917
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-15 18:31:31 +0100

    add lemma for equality of Z (equivalence relation)

--- Artifacts/Copy/Predicates_Z.h
+++ Artifacts/Copy/Predicates_Z.h
@@ -11,3 +11,14 @@
     IsEqual_Z{A,B}(Z* a, integer n, Z* b) =
       \forall integer i; 0 <= i < n  ==>  \at(a[i], A) == \at(b[i], B);
 */
+
+/*@
+  lemma Z_Eq_reflexiv:
+    \forall Z z; z == z;
+
+  lemma Z_Eq_symmetric:
+    \forall Z z1, z2; z1 == z2  ==>  z2 == z1;
+
+  lemma Z_Eq_transitiv:
+    \forall Z z1, z2, z3; (z1 == z2 && z2 == z3)  ==>  z1 == z3;
+*/
--- Artifacts/Copy/copy_Z.c
+++ Artifacts/Copy/copy_Z.c
@@ -11,6 +11,12 @@ void copy_Z(const Z* a, unsigned int n, Z* b)
   */
   for (unsigned int i = 0; i < n; ++i)
   {
+    /*@
+        assigns b[i];
+        ensures b[i] == \old(a[i]);
+        ensures \forall integer k; 0 <= k < i ==> a[k] == \old(a[k]);
+        ensures \forall integer k; 0 <= k < i ==> b[k] == \old(b[k]);
+    */
     b[i] = a[i];
   }
 }
