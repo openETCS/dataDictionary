219dc27ca4f8897681ba67bf60757f0965a87088
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-15 21:28:20 +0100

    finish outline of proof

--- Artifacts/Copy/copy_Z.c
+++ Artifacts/Copy/copy_Z.c
@@ -5,7 +5,8 @@ void copy_Z(const Z* a, unsigned int n, Z* b)
 {
   /*@
     loop invariant 0 <= i <= n;
-    loop invariant IsEqual_Z{Here,Here}(a, i, b);
+    loop invariant \forall integer k; 0 <= k < i ==> a[k] == b[k];
+    //loop invariant IsEqual_Z{Here,Here}(a, i, b);
     loop assigns   i, b[0..n-1];
     loop variant n-i;
   */
