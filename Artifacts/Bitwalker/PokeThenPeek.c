bd68d5f58984b4cf95401a901c3b1270ac4e815e
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-16 15:01:13 +0100

    simplify PokeThenPeek

--- Artifacts/Bitwalker/PokeThenPeek.c
+++ Artifacts/Bitwalker/PokeThenPeek.c
@@ -6,13 +6,11 @@
 uint64_t PokeThenPeek(uint8_t* addr, uint32_t size, uint32_t start, uint32_t length, uint64_t value)
 {
   //@ assert bits_covered: \forall integer i; 0 <= i < 64-length ==> !LeftBit64(value, i);
-  int      poke_result = 0;
-  uint64_t peek_result = 0u;
 
-  poke_result =  Bitwalker_Poke(addr, size, start, length, value);
+  int poke_result =  Bitwalker_Poke(addr, size, start, length, value);
   //@ assert result_zero:  poke_result == 0;
 
-  peek_result = Bitwalker_Peek(addr, size, start, length);
+  uint64_t peek_result = Bitwalker_Peek(addr, size, start, length);
 
   /*@ assert partial_copy: \forall integer i; 64-length <= i < 64 ==>
              (LeftBit64(peek_result, i) <==> LeftBit64(value, i)); */
