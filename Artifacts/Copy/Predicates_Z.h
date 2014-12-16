02aa0c73cf0eef6f27c7339f0db453bdbc10cadd
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-16 09:16:06 +0100

    rename lemma

--- Artifacts/Copy/Predicates_Z.h
+++ Artifacts/Copy/Predicates_Z.h
@@ -13,12 +13,12 @@
 */
 
 /*@
-  lemma Z_Eq_reflexiv:
+  lemma Z_Eq_reflexive:
     \forall Z z; z == z;
 
   lemma Z_Eq_symmetric:
     \forall Z z1, z2; z1 == z2  ==>  z2 == z1;
 
-  lemma Z_Eq_transitiv:
+  lemma Z_Eq_transitive:
     \forall Z z1, z2, z3; (z1 == z2 && z2 == z3)  ==>  z1 == z3;
 */
