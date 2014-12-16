15301b808c2d64614b42d58634a384f8d352ff93
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-16 14:46:15 +0100

    move declaration of return value

--- Artifacts/Bitwalker/PeekThenPoke.c
+++ Artifacts/Bitwalker/PeekThenPoke.c
@@ -5,14 +5,13 @@
 
 int PeekThenPoke(uint8_t* addr, uint32_t size, uint32_t start, uint32_t length)
 {
-  int result = 0;
   uint64_t value = Bitwalker_Peek(addr, size, start, length);
 
   /*@ assert copied:  \forall integer i; start <= i < start + length ==>
          (LeftBit8Array(addr, i) <==> LeftBit64(value, 64 - (start + length) + i));
    */
 
-  result =  Bitwalker_Poke(addr, size, start, length, value);
+  int result =  Bitwalker_Poke(addr, size, start, length, value);
 
   /*@ assert copied_unchanged:  \forall integer i; start <= i < start + length ==>
          (LeftBit8Array(addr, i) <==> \at(LeftBit8Array(addr, i), Pre));
