fcdd249debbc6598ddfaf844fab15a75d02d885e
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-17 10:55:10 +0100

    minor

--- Artifacts/Bitwalker/ReadThenWrite.c
+++ Artifacts/Bitwalker/ReadThenWrite.c
@@ -36,10 +36,10 @@ int ReadThenWrite(Bitstream* stream, uint32_t length)
   */
   uint64_t value = Bitstream_Read(stream, length);
 
-   // assert copied:  BitstreamEqual64(stream, \at(stream->bitpos, Pre), stream->bitpos, value);
-   // assert not_set: LeftNotSet64(value, 64 - length);
-   // assert value < (1 << length);
-   // assert stream->bitpos == \at(stream->bitpos, Pre) + length;
+   //@ assert copied:  BitstreamEqual64(stream, \at(stream->bitpos, Pre), stream->bitpos, value);
+   //@ assert not_set: LeftNotSet64(value, 64 - length);
+   //@ assert value < (1 << length);
+   //@ assert stream->bitpos == \at(stream->bitpos, Pre) + length;
 
   /*@
       requires \valid(stream);
