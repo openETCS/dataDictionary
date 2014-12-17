1c6a4f99969417646f7837825461ee1d576ceaa3
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-17 08:41:32 +0100

    continue working on ReadThenWrite

--- Artifacts/Bitwalker/ReadThenWrite.c
+++ Artifacts/Bitwalker/ReadThenWrite.c
@@ -6,24 +6,37 @@
     requires valid_stream: \valid(stream);
     requires stream_inv:   BitstreamInvariant(stream);
     requires max_length:   length <= 64;
-    requires max_pos:      NormalBitsequence(stream, length);
+    requires max_pos:      NormalBitsequence(stream, 2*length);
 
     assigns stream->addr[0..stream->size-1];
     assigns stream->bitpos;
 
     ensures result_zero: \result == 0;
     ensures unchanged:   BitstreamUnchanged{Old}(stream, 0, 8 * stream->size);
+    ensures pos:         stream->bitpos == 2 * \old(stream->bitpos);
 */
 int ReadThenWrite(Bitstream* stream, uint32_t length)
 {
-  int result = 0;
   uint64_t value = Bitstream_Read(stream, length);
 
    //@ assert copied:  BitstreamEqual64(stream, \at(stream->bitpos, Pre), stream->bitpos, value);
    //@ assert not_set: LeftNotSet64(value, 64 - length);
    //@ assert value < (1 << length);
-  result =  Bitstream_Write(stream, length, value);
+   //@ assert stream->bitpos == \at(stream->bitpos, Pre) + length;
 
+  /*@
+      requires \valid(stream);
+      requires BitstreamInvariant(stream);
+      requires length <= 64;
+      requires NormalBitsequence(stream, length) && value < (1 << length);
+
+      assigns stream->addr[0..stream->size-1];
+      assigns stream->bitpos;
+
+      ensures stream->bitpos == \old(stream->bitpos) + length;
+      ensures result == 0;
+  */
+  int result =  Bitstream_Write(stream, length, value);
 
   //@ assert unchanged_left:  BitstreamUnchanged{Pre}(stream, 0, \at(stream->bitpos, Pre));
   //@ assert copied:          BitstreamEqual64(stream, \at(stream->bitpos, Pre), stream->bitpos, value);
