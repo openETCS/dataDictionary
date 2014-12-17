0e180bb6016282bbcdc460d8d4281ee8097ee8c6
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-17 09:28:48 +0100

    save current state

--- Artifacts/Bitwalker/ReadThenWrite.c
+++ Artifacts/Bitwalker/ReadThenWrite.c
@@ -6,6 +6,7 @@
     requires valid_stream: \valid(stream);
     requires stream_inv:   BitstreamInvariant(stream);
     requires max_length:   length <= 64;
+    requires max_pos:      stream->bitpos + length <= UINT32_MAX;
     requires max_pos:      NormalBitsequence(stream, 2*length);
 
     assigns stream->addr[0..stream->size-1];
@@ -17,21 +18,39 @@
 */
 int ReadThenWrite(Bitstream* stream, uint32_t length)
 {
+  /*@
+     requires valid_stream:     \valid(stream);
+     requires stream_invariant: BitstreamInvariant(stream);
+     requires max_length:       length <= 64;
+     requires max_pos:          stream->bitpos + length <= UINT32_MAX;
+     requires max_pos:          NormalBitsequence(stream, length);
+
+     assigns  stream->bitpos;
+     assigns  value;
+
+     ensures increment:     stream->bitpos == \old(stream->bitpos) + length;
+     ensures copied:        BitstreamEqual64(stream, \old(stream->bitpos), stream->bitpos, value);
+     ensures not_set:       LeftNotSet64(value, 64 - length);
+     ensures unchanged:     BitstreamUnchanged{Old}(stream, 0, 8*stream->size);
+     ensures valid_result:  value < (1 << length);
+  */
   uint64_t value = Bitstream_Read(stream, length);
 
-   //@ assert copied:  BitstreamEqual64(stream, \at(stream->bitpos, Pre), stream->bitpos, value);
-   //@ assert not_set: LeftNotSet64(value, 64 - length);
-   //@ assert value < (1 << length);
-   //@ assert stream->bitpos == \at(stream->bitpos, Pre) + length;
+   // assert copied:  BitstreamEqual64(stream, \at(stream->bitpos, Pre), stream->bitpos, value);
+   // assert not_set: LeftNotSet64(value, 64 - length);
+   // assert value < (1 << length);
+   // assert stream->bitpos == \at(stream->bitpos, Pre) + length;
 
   /*@
       requires \valid(stream);
       requires BitstreamInvariant(stream);
       requires length <= 64;
-      requires NormalBitsequence(stream, length) && value < (1 << length);
+      requires NormalBitsequence(stream, length);
+      requires value < (1 << length);
 
       assigns stream->addr[0..stream->size-1];
       assigns stream->bitpos;
+      assigns result;
 
       ensures stream->bitpos == \old(stream->bitpos) + length;
       ensures result == 0;
