4eb0a0465e0038e6c4b5f8a16476361dab4aae24
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-18 09:29:07 +0100

    remove statement contracts

--- Artifacts/Bitwalker/ReadThenWrite.c
+++ Artifacts/Bitwalker/ReadThenWrite.c
@@ -12,28 +12,10 @@
     assigns stream->addr[0..stream->size-1];
     assigns stream->bitpos;
 
-    //ensures result_zero: \result == 0;
     ensures unchanged:   BitstreamUnchanged{Old}(stream, 0, 8 * stream->size);
-    //ensures pos:         stream->bitpos == \old(stream->bitpos) + 2*length;
 */
 int ReadThenWrite(Bitstream* stream, uint32_t length)
 {
-  /*
-     requires valid_stream:     \valid(stream);
-     requires stream_invariant: BitstreamInvariant(stream);
-     requires max_length:       length <= 64;
-     requires max_pos:          stream->bitpos + length <= UINT32_MAX;
-     requires max_pos:          NormalBitsequence(stream, length);
-
-     assigns  stream->bitpos;
-     assigns  value;
-
-     ensures increment:     stream->bitpos == \old(stream->bitpos) + length;
-     ensures copied:        BitstreamEqual64(stream, \old(stream->bitpos), stream->bitpos, value);
-     ensures not_set:       LeftNotSet64(value, 64 - length);
-     ensures unchanged:     BitstreamUnchanged{Old}(stream, 0, 8*stream->size);
-     ensures valid_result:  value < (1 << length);
-  */
   uint64_t value = Bitstream_Read(stream, length);
 
    //@ assert copied:  BitstreamEqual64(stream, \at(stream->bitpos, Pre), stream->bitpos, value);
@@ -41,26 +23,11 @@ int ReadThenWrite(Bitstream* stream, uint32_t length)
    //@ assert value < (1 << length);
    //@ assert stream->bitpos == \at(stream->bitpos, Pre) + length;
 
-  /*@
-      requires \valid(stream);
-      requires BitstreamInvariant(stream);
-      requires length <= 64;
-      requires NormalBitsequence(stream, length);
-      requires value < (1 << length) || length == 64;
-
-      assigns stream->addr[0..stream->size-1];
-      assigns stream->bitpos;
-      assigns result;
-
-      ensures stream->bitpos == \old(stream->bitpos) + length;
-      //ensures result == 0;
-  */
   int result =  Bitstream_Write(stream, length, value);
 
   //@ assert unchanged_left:  BitstreamUnchanged{Pre}(stream, 0, \at(stream->bitpos, Pre));
   //@ assert copied:          BitstreamEqual64(stream, \at(stream->bitpos, Pre), stream->bitpos, value);
   //@ assert unchanged_right: BitstreamUnchanged{Pre}(stream, stream->bitpos, 8 * stream->size);
-  // assert zero:            result == 0;
 
   return result;
 }
