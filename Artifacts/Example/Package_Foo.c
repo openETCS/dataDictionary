1ddc36412ee4ef037071f51855f2c339f19c64e7
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-20 22:04:42 +0100

    use ghost code for more compact name

--- Artifacts/Example/Package_Foo.c
+++ Artifacts/Example/Package_Foo.c
@@ -13,14 +13,22 @@ int Package_Foo_Decoder(Bitstream* stream, Package_Foo* p)
 {
   if (stream->bitpos + 28 <= 8 * stream->size)
   {
+    //@ ghost uint32_t pos = stream->bitpos;
+
     p->ABC = Bitstream_Read(stream, 8);
-    //@ assert stream->bitpos == \at(stream->bitpos, Pre) + 8;
+    //@ assert stream->bitpos == pos + 8;
+    //@ assert BitstreamEqual64(stream, pos, pos + 8,  p->ABC);
+    //@ assert UpperBitsNotSet(p->ABC, 8);
 
     p->DEF = Bitstream_Read(stream, 3);
-    //@ assert stream->bitpos == \at(stream->bitpos, Pre) + 11;
+    //@ assert stream->bitpos == pos + 11;
+    //@ assert BitstreamEqual64(stream, pos + 8,  pos + 11,  p->DEF);
+    //@ assert UpperBitsNotSet(p->ABC, 3);
 
     p->GHI = Bitstream_Read(stream, 17);
-    //@ assert stream->bitpos == \at(stream->bitpos, Pre) + 28;
+    //@ assert stream->bitpos == pos + 28;
+    //@ assert BitstreamEqual64(stream, pos + 11,  pos + 28,  p->GHI);
+    //@ assert UpperBitsNotSet(p->ABC, 17);
 
     return 1;
   }
