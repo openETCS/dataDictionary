284181fe34a0c72f8d71051024a0e4570f5f81d1
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-20 19:28:48 +0100

    minor changes

--- Artifacts/Example/Package_Foo.h
+++ Artifacts/Example/Package_Foo.h
@@ -50,10 +50,11 @@ void Package_Foo_Init(Package_Foo* p);
       assigns *p;
 
       ensures copied:  BitstreamEqual64(stream, \old(stream->bitpos),      \old(stream->bitpos) + 8,  p->ABC);
-      ensures copied:  BitstreamEqual64(stream, \old(stream->bitpos) + 8,  \old(stream->bitpos) + 11, p->DEF);
-      ensures copied:  BitstreamEqual64(stream, \old(stream->bitpos) + 11, \old(stream->bitpos) + 28, p->GHI);
+      ensures not_set: UpperBitsNotSet(p->ABC, 8);
 
-      //ensures not_set:      LeftNotSet64(\result, 64 - length);
+      //ensures copied:  BitstreamEqual64(stream, \old(stream->bitpos) + 8,  \old(stream->bitpos) + 11, p->DEF);
+      //ensures copied:  BitstreamEqual64(stream, \old(stream->bitpos) + 11, \old(stream->bitpos) + 28, p->GHI);
+
 
       ensures stream->bitpos == \old(stream->bitpos) + 28;
       ensures \result == 1;
