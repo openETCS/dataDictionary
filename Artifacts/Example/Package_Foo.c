76074112b1fe6d608675d491562c2e5aca0a6cae
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-20 12:40:25 +0100

    provide first version of Decoder

--- Artifacts/Example/Package_Foo.c
+++ Artifacts/Example/Package_Foo.c
@@ -8,3 +8,25 @@ void Package_Foo_Init(Package_Foo* p)
     p->GHI = 0;
 }
 
+
+int Package_Foo_Decoder(Bitstream* stream, Package_Foo* p)
+{
+    if (stream->bitpos + 28 <= 8 * stream->size)
+    {
+        p->ABC = Bitstream_Read(stream, 8);
+        // assert stream->bitpos == \at(stream->bitpos, Pre) + 8;
+
+        p->DEF = Bitstream_Read(stream, 3);
+        // assert stream->bitpos == \at(stream->bitpos, Pre) + 11;
+
+        p->GHI = Bitstream_Read(stream, 17);
+        // assert stream->bitpos == \at(stream->bitpos, Pre) + 28;
+
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
--- Artifacts/Example/Package_Foo.h
+++ Artifacts/Example/Package_Foo.h
@@ -2,17 +2,22 @@
 #ifndef PACKAGE_FOO_H_INCLUDED
 #define PACKAGE_FOO_H_INCLUDED
 
-#include "FramaCBits.h"
+#include "Bitstream_Read.h"
 
 struct Package_Foo
 {
     uint64_t ABC;  // 8
     uint64_t DEF;  // 3
     uint64_t GHI;  // 17
+                   // total: 28 
 };
 
-
 typedef struct Package_Foo Package_Foo;
+
+//static inline uint32_t Package_Foo_Bits(const Package_Foo* p)
+//{
+ //   return 28;
+//}
 
 /*@
     requires \valid(p);
@@ -24,5 +29,38 @@ typedef struct Package_Foo Package_Foo;
     ensures  p->GHI == 0;
 */
 void Package_Foo_Init(Package_Foo* p);
+
+/*@
+    requires \valid(stream);
+    requires BitstreamInvariant(stream);
+    requires stream->bitpos + 28 <= UINT32_MAX;
+    requires \valid(p);
+    requires \separated(stream, p);
+    requires \separated(stream->addr + (0..stream->size-1), p);
+
+    assigns stream->bitpos;
+    assigns *p;
+
+    behavior normal_case:
+      assumes stream->bitpos + 28 <= 8 * stream->size;
+
+      assigns stream->bitpos;
+      assigns *p;
+
+      ensures stream->bitpos == \old(stream->bitpos) + 28;
+      ensures \result == 1;
+
+    behavior error_case:
+      assumes stream->bitpos + 28 > 8 * stream->size;
+
+      assigns \nothing;
+
+      ensures \result == 0;
+
+    complete behaviors;
+    disjoint behaviors;
+*/
+int Package_Foo_Decoder(Bitstream* stream, Package_Foo* p);
+
 
 #endif // PACKAGE_FOO_H_INCLUDED
