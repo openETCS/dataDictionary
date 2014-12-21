48fee14b819b91b1e9f6f6b65c9b40827314ca41
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-21 19:16:56 +0100

    add labels to predicates but no change to proof

--- Artifacts/Bitwalker/BitstreamInvariant.h
+++ Artifacts/Bitwalker/BitstreamInvariant.h
@@ -5,12 +5,12 @@
 #include "Bitstream.h"
 
 /*@
-  predicate BitstreamInvariant(Bitstream* stream) =
+  predicate BitstreamInvariant{L}(Bitstream* stream) =
      \valid(stream->addr + (0..stream->size-1))  &&
      \separated(stream, stream->addr + (0..stream->size-1)) &&
      8 * stream->size <= UINT32_MAX;
 
-  predicate NormalBitsequence(Bitstream* stream, integer length) =
+  predicate NormalBitsequence{L}(Bitstream* stream, integer length) =
      stream->bitpos + length <= 8 * stream->size;
 
   predicate LeftBitInStream{L}(Bitstream* stream, integer i) = LeftBit8Array(stream->addr, i);
