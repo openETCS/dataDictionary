95164c47825bf51fb2a8acc3181f3c110f05173b
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-21 11:57:33 +0100

    add formatting option -m0

--- Artifacts/Example/Package_Foo_UpperBitsNotSet.c
+++ Artifacts/Example/Package_Foo_UpperBitsNotSet.c
@@ -5,8 +5,8 @@
 int Package_Foo_UpperBitsNotSet(const Package_Foo* p)
 {
     if (UpperBitsNotSet64(p->ABC, 8) &&
-            UpperBitsNotSet64(p->DEF, 3) &&
-            UpperBitsNotSet64(p->GHI, 17))
+        UpperBitsNotSet64(p->DEF, 3) &&
+        UpperBitsNotSet64(p->GHI, 17))
     {
         return 1;
     }
