da35ce5a9182d2363e47b0982b634ee8bd20332a
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-21 18:55:17 +0100

    remove statement contracts

--- Artifacts/Bitwalker/ReadThenWrite.c
+++ Artifacts/Bitwalker/ReadThenWrite.c
@@ -16,21 +16,6 @@
 */
 void ReadThenWrite(Bitstream* stream, const uint32_t length)
 {
-    /*
-       requires valid_stream:     \valid(stream);
-       requires stream_invariant: BitstreamInvariant(stream);
-       requires max_length:       length <= 64;
-       requires max_pos:          stream->bitpos + length <= UINT32_MAX;
-       requires max_pos:          NormalBitsequence(stream, length);
-
-       assigns  stream->bitpos;
-       assigns  value;
-
-       ensures increment:     stream->bitpos == \old(stream->bitpos) + length;
-       ensures copied:        BitstreamEqual64{Pre}(stream, \old(stream->bitpos), stream->bitpos, value);
-       ensures not_set:       UpperBitsNotSet(value, length);
-       ensures unchanged:     BitstreamUnchanged{Pre}(stream, 0, 8*stream->size);
-    */
     uint64_t value = Bitstream_Read(stream, length);
 
     //@ assert copied1:      BitstreamEqual64(stream, \at(stream->bitpos, Pre), stream->bitpos, value);
@@ -41,18 +26,6 @@ void ReadThenWrite(Bitstream* stream, const uint32_t length)
     //@ assert stream->bitpos == \at(stream->bitpos, Pre);
     //@ assert NormalBitsequence(stream, length);
 
-    /*
-        requires \valid(stream);
-        requires BitstreamInvariant(stream);
-        requires length <= 64;
-        requires NormalBitsequence(stream, length);
-        requires UpperBitsNotSet(value, length);
-
-        assigns stream->addr[0..stream->size-1];
-        assigns stream->bitpos;
-
-        ensures stream->bitpos == \old(stream->bitpos) + length;
-    */
     Bitstream_Write(stream, length, value);
 
     //@ assert unchanged_left:  BitstreamUnchanged{Pre}(stream, 0, \at(stream->bitpos, Pre));
