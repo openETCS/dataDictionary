4f69ab3d5b9d73ed7d9fc6a74a7fc06eaf0021a8
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-21 11:31:46 +0100

    use helper function in Encoder

--- Artifacts/Example/Package_Foo_Encoder.c
+++ Artifacts/Example/Package_Foo_Encoder.c
@@ -1,20 +1,28 @@
 
 #include "Package_Foo_Encoder.h"
+#include "Package_Foo_UpperBitsNotSet.h"
 #include "Bitstream_Write.h"
 
 int Package_Foo_Encoder(Bitstream* stream, const Package_Foo* p)
 {
   if (stream->bitpos + 28 <= 8 * stream->size)
   {
-     Bitstream_Write(stream, 8, p->ABC);
-     Bitstream_Write(stream, 3, p->DEF);
-     Bitstream_Write(stream, 17, p->GHI);
+     if (Package_Foo_UpperBitsNotSet(p))
+     {
+         Bitstream_Write(stream, 8, p->ABC);
+         Bitstream_Write(stream, 3, p->DEF);
+         Bitstream_Write(stream, 17, p->GHI);
 
-     return 1;
+         return 1;
+     }
+     else
+     {
+         return -2;
+     }
   }
   else
   {
-     return 0;
+     return -1;
   }
 }
 
