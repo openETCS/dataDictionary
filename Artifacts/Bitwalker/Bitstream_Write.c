ebbf61601caea0a5005cd1824d6f4f8852e7a15c
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-25 09:56:03 +0100

    add assertion and use it to simplify proof

--- Artifacts/Bitwalker/Bitstream_Write.c
+++ Artifacts/Bitwalker/Bitstream_Write.c
@@ -4,6 +4,7 @@
 
 int Bitstream_Write(Bitstream* stream, uint32_t length, uint64_t value)
 {
+    //@ assert \separated(stream, stream->addr + (0..stream->size-1));
     int retval = Bitwalker_Poke(stream->addr, stream->size, stream->bitpos, length, value);
 
     //@ assert bitpos_unchanged: stream->bitpos == \at(stream->bitpos, Pre);
