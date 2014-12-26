1185672a351274c3fd8432490f1c0396a951d32b
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-26 11:41:02 +0100

    simply verification of PokeThenPeek

--- Artifacts/Bitwalker/PokeThenPeek.c
+++ Artifacts/Bitwalker/PokeThenPeek.c
@@ -9,13 +9,10 @@ uint64_t PokeThenPeek(uint8_t* addr, uint32_t size, uint32_t start, uint32_t len
 
     uint64_t peek_result = Bitwalker_Peek(addr, size, start, length);
 
-    /*@ assert partial_copy: \forall integer i; 64-length <= i < 64 ==>
-               (LeftBit64(peek_result, i) <==> LeftBit64(value, i)); */
+    /*@ assert partial_copy: \forall integer i; 0 <= i < length ==>
+               (BitTest(peek_result, i) <==> BitTest(value, i)); */
 
     /*@ assert full_copy:  \forall integer i; 0 <= i < 64 ==>
-               (LeftBit64(peek_result, i) <==> LeftBit64(value, i)); */
-
-    /*@ assert full_copy2:  \forall integer i; 0 <= i < 64 ==>
                (BitTest(peek_result, i) <==> BitTest(value, i)); */
 
     return peek_result;
