69404c5fa5395551af005134773d12e91f958dde
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-30 12:08:47 +0100

    add post condition on invariant

--- Artifacts/Bitwalker/Bitstream_Read.h
+++ Artifacts/Bitwalker/Bitstream_Read.h
@@ -6,32 +6,34 @@
 #include "BitstreamInvariant.h"
 
 /*@
-  requires valid_stream:     \valid(stream);
-  requires stream_invariant: BitstreamInvariant(stream);
-  requires max_pos:          stream->bitpos + length <= UINT32_MAX;
-  requires max_length:       length <= 64;
+  requires valid_stream: \valid(stream);
+  requires invariant:    BitstreamInvariant(stream);
+  requires max_pos:      stream->bitpos + length <= UINT32_MAX;
+  requires max_length:   length <= 64;
 
   assigns  stream->bitpos;
 
   ensures  increment: stream->bitpos == \old(stream->bitpos) + length;
 
   behavior  invalid_bit_sequence:
-    assumes  !NormalBitsequence{Pre}(stream, length);
+    assumes !NormalBitsequence{Pre}(stream, length);
 
-    assigns  stream->bitpos;
+    assigns stream->bitpos;
 
     ensures invalid_result: \result == 0;
 
   behavior  normal_case:
-    assumes  NormalBitsequence{Pre}(stream, length); // it is strange to add "Pre" here
+    assumes NormalBitsequence{Pre}(stream, length); // it is strange to add "Pre" here
 
-    assigns  stream->bitpos;
+    assigns stream->bitpos;
 
-    ensures copied:       BitstreamEqual64(stream, \old(stream->bitpos), stream->bitpos, \result);
+    ensures copied:    BitstreamEqual64(stream, \old(stream->bitpos), stream->bitpos, \result);
 
-    ensures not_set:      UpperBitsNotSet(\result, length);
+    ensures not_set:   UpperBitsNotSet(\result, length);
 
-    ensures unchanged:    BitstreamUnchanged{Here,Old}(stream, 0, 8*stream->size);
+    ensures unchanged: BitstreamUnchanged{Here,Old}(stream, 0, 8*stream->size);
+
+    ensures invariant: BitstreamInvariant(stream);
 
   complete behaviors;
   disjoint behaviors;
