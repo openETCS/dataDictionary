40bbabed61b11658ec2b2e0e23013e39fbaff882
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-30 12:14:42 +0100

    add postcodition on invariant of stream

--- Artifacts/Bitwalker/Bitstream_Write.h
+++ Artifacts/Bitwalker/Bitstream_Write.h
@@ -17,34 +17,36 @@
   ensures  increment: stream->bitpos == \old(stream->bitpos) + length;
 
   behavior  invalid_bit_sequence:
-    assumes  !NormalBitsequence{Pre}(stream, length);
+    assumes !NormalBitsequence{Pre}(stream, length);
 
-    assigns  stream->addr[0..stream->size - 1];
-    assigns  stream->bitpos;
+    assigns stream->addr[0..stream->size - 1];
+    assigns stream->bitpos;
 
-    ensures valid_result:  \result == -1;
+    ensures result:  \result == -1;
 
   behavior  value_too_big:
     assumes NormalBitsequence{Pre}(stream, length) && !UpperBitsNotSet(value, length);
 
-    assigns  stream->addr[0..stream->size - 1];
-    assigns  stream->bitpos;
+    assigns stream->addr[0..stream->size - 1];
+    assigns stream->bitpos;
 
-    ensures wrong_value_result: \result == -2;
+    ensures result: \result == -2;
 
   behavior  normal_case:
     assumes NormalBitsequence{Pre}(stream, length) && UpperBitsNotSet{Pre}(value, length);
 
-    assigns  stream->addr[0..stream->size - 1];
-    assigns  stream->bitpos;
+    assigns stream->addr[0..stream->size - 1];
+    assigns stream->bitpos;
 
-    ensures unchanged_left:  BitstreamUnchanged{Here,Old}(stream, 0, \old(stream->bitpos));
+    ensures left:   BitstreamUnchanged{Here,Old}(stream, 0, \old(stream->bitpos));
 
-    ensures copied: BitstreamEqual64(stream, \old(stream->bitpos), stream->bitpos, value);
+    ensures middle: BitstreamEqual64(stream, \old(stream->bitpos), stream->bitpos, value);
 
-    ensures unchanged_right: BitstreamUnchanged{Here,Old}(stream, stream->bitpos, 8 * stream->size);
+    ensures right:  BitstreamUnchanged{Here,Old}(stream, stream->bitpos, 8 * stream->size);
 
-    ensures valid_result:    \result == 0;
+    ensures result: \result == 0;
+
+    ensures BitstreamInvariant(stream);
 
   complete behaviors;
   disjoint behaviors;
