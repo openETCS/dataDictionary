4938a47718a6fcc62365fc0e54fadfcd2342d70f
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-30 19:36:05 +0100

    use Read_Normal in Decoder

--- Artifacts/Package_Foo/Package_Foo_Decoder.c
+++ Artifacts/Package_Foo/Package_Foo_Decoder.c
@@ -1,6 +1,6 @@
 
 #include "Package_Foo_Decoder.h"
-#include "Bitstream_Read.h"
+#include "Bitstream_Read_Normal.h"
 
 int Package_Foo_Decoder(Bitstream* stream, Package_Foo* p)
 {
@@ -23,7 +23,7 @@ int Package_Foo_Decoder(Bitstream* stream, Package_Foo* p)
             ensures ABC_equal:  BitstreamEqual64(stream, pos, pos + 8,  p->ABC);
             ensures ABC_upper:  UpperBitsNotSet(p->ABC, 8);
         */
-        p->ABC = Bitstream_Read(stream, 8);
+        p->ABC = Bitstream_Read_Normal(stream, 8);
 
         /*@
             requires DEF_normal: Normal{Here}(stream, 3);
@@ -35,7 +35,7 @@ int Package_Foo_Decoder(Bitstream* stream, Package_Foo* p)
             ensures DEF_equal:  BitstreamEqual64(stream, pos + 8, pos + 11,  p->DEF);
             ensures DEF_upper:  UpperBitsNotSet(p->DEF, 3);
         */
-        p->DEF = Bitstream_Read(stream, 3);
+        p->DEF = Bitstream_Read_Normal(stream, 3);
 
         /*@
             requires GHI_normal: Normal(stream, 17);
@@ -47,7 +47,7 @@ int Package_Foo_Decoder(Bitstream* stream, Package_Foo* p)
             ensures GHI_equal:  BitstreamEqual64(stream, pos + 11, pos + 28,  p->GHI);
             ensures GHI_upper:  UpperBitsNotSet(p->GHI, 17);
         */
-        p->GHI = Bitstream_Read(stream, 17);
+        p->GHI = Bitstream_Read_Normal(stream, 17);
 
         //@ assert ABC_final:  BitstreamEqual64(stream, pos, pos + 8,  p->ABC);
         //@ assert DEF_final:  BitstreamEqual64(stream, pos + 8, pos + 11,  p->DEF);
