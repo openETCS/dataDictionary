0140e323b8f27ee76d0e8fe2908d2b66fec26330
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-31 12:11:00 +0100

    rename left -> pos

--- Artifacts/Bitwalker/Bit8Array.c
+++ Artifacts/Bitwalker/Bit8Array.c
@@ -2,32 +2,32 @@
 #include "Bit8Array.h"
 #include "Bit8.h"
 
-int PeekBit8Array(uint8_t*  addr, uint32_t size, uint32_t left)
+int PeekBit8Array(uint8_t*  addr, uint32_t size, uint32_t pos)
 {
-    return PeekBit8(addr[left / 8], left % 8);
+    return PeekBit8(addr[pos / 8], pos % 8);
 }
 
 
-void PokeBit8Array(uint8_t* addr, uint32_t size, uint32_t left, int flag)
+void PokeBit8Array(uint8_t* addr, uint32_t size, uint32_t pos, int flag)
 {
-    uint32_t i = left / 8u;
-    uint32_t k = left % 8u;
+    uint32_t i = pos / 8u;
+    uint32_t k = pos % 8u;
 
     addr[i] = PokeBit8(addr[i], k, flag);
 
-    // The following assertion claims that in byte with index "left/8"
+    // The following assertion claims that in byte with index "pos/8"
     // the bits with indices different from "k" do not change
     /*@
       assert bits_in_byte:
         \forall integer j; (0 <= j < 8  && j != k) ==>
-        (LeftBit8(addr[left/8], j) <==> \at(LeftBit8(addr[left/8], j), Pre));
+        (LeftBit8(addr[pos/8], j) <==> \at(LeftBit8(addr[pos/8], j), Pre));
     */
 
     // The following assertion claims that in every byte
-    // with an index that is different from "left/8" no bit is changed.
+    // with an index that is different from "pos/8" no bit is changed.
     /*@
       assert bits_in_other_bytes:
-        \forall integer l, j; (0 <= l < size  &&  l != left/8  &&  0 <= j < 8) ==>
+        \forall integer l, j; (0 <= l < size  &&  l != pos/8  &&  0 <= j < 8) ==>
           (LeftBit8(addr[l], j) <==> \at(LeftBit8(addr[l], j), Pre));
     */
 
--- Artifacts/Bitwalker/Bit8Array.h
+++ Artifacts/Bitwalker/Bit8Array.h
@@ -6,34 +6,34 @@
 
 /*@
     requires array_length:  \valid_read(addr + (0..size-1));
-    requires bit_size:      8*size < UINT32_MAX;
-    requires bit_index:     left < 8*size;
+    requires bit_size:      8 * size < UINT32_MAX;
+    requires bit_index:     pos < 8 * size;
 
     assigns \nothing;
 
-    ensures set_bit:  \result != 0 <==> LeftBit8Array(addr, left);
+    ensures set_bit:  \result != 0 <==> LeftBit8Array(addr, pos);
 */
-int PeekBit8Array(uint8_t*  addr, uint32_t  size, uint32_t  left);
+int PeekBit8Array(uint8_t*  addr, uint32_t  size, uint32_t  pos);
 
 
 
 /*@
     requires array_length:  \valid(addr + (0..size-1));
     requires bit_size:      8*size < UINT32_MAX;
-    requires bit_index:     left < 8*size;
+    requires bit_index:     pos < 8*size;
 
     assigns addr[0..size-1];
 
     ensures unchanged_bytes:
-         \forall integer i; 0 <= i < size  &&  i != left/8 ==> addr[i] == \old(addr[i]);
+         \forall integer i; 0 <= i < size  &&  i != pos/8 ==> addr[i] == \old(addr[i]);
 
     ensures unchanged_bits:
-         \forall integer i; 0 <= i < 8 * size  &&  i != left ==>
+         \forall integer i; 0 <= i < 8 * size  &&  i != pos ==>
                 ((LeftBit8Array(addr, i) <==> \old(LeftBit8Array(addr, i))));
 
-    ensures set_bit:   LeftBit8Array(addr, left) <==> (flag != 0);
+    ensures set_bit:   LeftBit8Array(addr, pos) <==> (flag != 0);
 */
-void PokeBit8Array(uint8_t* addr, uint32_t size, uint32_t left, int flag);
+void PokeBit8Array(uint8_t* addr, uint32_t size, uint32_t pos, int flag);
 
 #endif // BIT8ARRAY_H_defined
 
