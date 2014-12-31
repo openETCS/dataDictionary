bd20830f2033d2866efa18648cdb76f6935ffff9
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-31 14:14:18 +0100

    rewrite loop (start .. start + length)

--- Artifacts/Bitwalker/Bitwalker_Poke_Normal.c
+++ Artifacts/Bitwalker/Bitwalker_Poke_Normal.c
@@ -5,22 +5,22 @@
 void Bitwalker_Poke_Normal(uint8_t* addr, uint32_t size, uint32_t start, uint32_t length, uint64_t value)
 {
     /*@
-      loop invariant index:  0 <= i <= length;
+      loop invariant index:  start <= i <= start + length;
 
       loop invariant left:   BitsUnchanged{Here,Pre}(addr, 0, start);
 
-      loop invariant middle: EqualBits64(addr, start, start + i, value, length);
+      loop invariant middle: EqualBits64(addr, start, i, value, length);
 
-      loop invariant right:  BitsUnchanged{Here,Pre}(addr, start + i, 8 * size);
+      loop invariant right:  BitsUnchanged{Here,Pre}(addr, i, 8 * size);
 
       loop assigns i, addr[0..size-1];
 
-      loop variant length - i;
+      loop variant start + length - i;
     */
-    for (uint32_t i = 0; i < length; ++i)
+    for (uint32_t i = start; i < start + length; ++i)
     {
-        int flag = PeekBit64(value, (64 - length) + i);
-        PokeBit8Array(addr, size, start + i, flag);
+        int flag = PeekBit64(value, (64 - length) + (i - start));
+        PokeBit8Array(addr, size, start + (i - start), flag);
     }
 }
 
