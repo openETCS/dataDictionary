7a20d17a81b126b926ba1ce53137eb28c3b1859a
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-12-31 13:12:59 +0100

    eliminate coq proof for Bitstream_Write_Normal

--- Artifacts/Bitwalker/Bitstream_Write_Normal.c
+++ Artifacts/Bitwalker/Bitstream_Write_Normal.c
@@ -4,10 +4,9 @@
 
 void Bitstream_Write_Normal(Bitstream* stream, uint32_t length, uint64_t value)
 {
-    //@ assert \separated(stream, stream->addr + (0..stream->size-1));
     Bitwalker_Poke_Normal(stream->addr, stream->size, stream->bitpos, length, value);
-
-    //@ assert bitpos_unchanged: stream->bitpos == \at(stream->bitpos, Pre);
+    //@ assert middle: EqualBits64(stream->addr, stream->bitpos, stream->bitpos + length, value);
+    
     stream->bitpos += length;
 }
 
