29b6376e09cc9b0f2cd9c164bb47dde8fd4ecac1
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2015-01-01 22:37:03 +0100

    use Bitwalker_Poke_Normal in Encoder (successfully)

--- Artifacts/Package_Foo/Package_Foo_Encoder.c
+++ Artifacts/Package_Foo/Package_Foo_Encoder.c
@@ -1,52 +1,45 @@
 
 #include "Package_Foo_Encoder.h"
 #include "Package_Foo_UpperBitsNotSet.h"
-#include "Bitstream_Write_Normal.h"
+#include "Bitwalker_Poke_Normal.h"
 
 int Package_Foo_Encoder(Bitstream* stream, const Package_Foo* p)
 {
-    if (stream->bitpos + 28 <= 8 * stream->size)
+    uint8_t* addr = stream->addr;
+    const uint32_t pos  = stream->bitpos;
+    const uint32_t size = stream->size;
+    if (pos + 28 <= 8 * size)
     {
         if (Package_Foo_UpperBitsNotSet(p))
         {
             //@ assert Normal(stream, 28);
-            //@ ghost uint32_t pos = stream->bitpos;
-            //@ ghost uint32_t size = stream->size;
             //@ assert \separated(stream, stream->addr + (0..stream->size-1));
+            //@ assert \separated(p,      stream->addr + (0..stream->size-1));
+            //@ assert \separated(p,      stream);
 
             /*@
                 requires ABC_normal: Normal(stream, 8);
                 requires ABC_upper:  UpperBitsNotSet(p->ABC, 8);
 
-                assigns stream->bitpos;
-                assigns stream->addr[0..(stream->size-1)];
+                assigns addr[0..(size-1)];
 
-                ensures ABC_bitpos: stream->bitpos == \old(stream->bitpos) + 8;
-                ensures ABC_size:   stream->size == \old(stream->size);
-                ensures ABC_left:   BitsUnchanged{Here,Old}(stream->addr, 0, pos);
-                ensures ABC_equal:  BitstreamEqual64(stream, pos, pos + 8,  p->ABC);
-                ensures ABC_right:  BitsUnchanged{Here,Old}(stream->addr, pos + 8, 8 * stream->size);
+                ensures ABC_left:   BitsUnchanged{Here,Old}(addr, 0, pos);
+                ensures ABC_equal:  EqualBits64(addr, pos, pos + 8,  p->ABC);
+                ensures ABC_right:  BitsUnchanged{Here,Old}(addr, pos + 8, 8 * size);
             */
-            Bitstream_Write_Normal(stream, 8, p->ABC);
-
-            //@ assert stream->size == size;
+            Bitwalker_Poke_Normal(addr, size, pos, 8, p->ABC);
 
             /*@
                 requires DEF_normal: Normal{Here}(stream, 3);
                 requires DEF_upper:  UpperBitsNotSet(p->DEF, 3);
 
-                assigns stream->bitpos;
-                assigns stream->addr[0..(stream->size-1)];
+                assigns addr[0..(size-1)];
 
-                ensures DEF_bitpos: stream->bitpos == \old(stream->bitpos) + 3;
-                ensures DEF_size:   stream->size == \old(stream->size);
-                ensures DEF_left:   BitsUnchanged{Here,Old}(stream->addr, 0, pos + 8);
-                ensures DEF_equal:  BitstreamEqual64(stream, pos + 8, pos + 11,  p->DEF);
-                ensures DEF_right:  BitsUnchanged{Here,Old}(stream->addr, pos + 11, 8 * stream->size);
-
-                //ensures ABC_DEF_equal: BitstreamEqual64(stream, pos, pos + 8,  p->ABC);
+                ensures DEF_left:   BitsUnchanged{Here,Old}(addr, 0, pos + 8);
+                ensures DEF_equal:  EqualBits64(addr, pos + 8, pos + 11,  p->DEF);
+                ensures DEF_right:  BitsUnchanged{Here,Old}(addr, pos + 11, 8 * size);
             */
-            Bitstream_Write_Normal(stream, 3, p->DEF);
+            Bitwalker_Poke_Normal(addr, size, pos + 8, 3, p->DEF);
             //@ ghost post_DEF:
 
             /*@
@@ -54,16 +47,15 @@ int Package_Foo_Encoder(Bitstream* stream, const Package_Foo* p)
                 requires GHI_upper:  UpperBitsNotSet(p->GHI, 17);
                 requires \separated(stream, p);
 
-                assigns stream->bitpos;
-                assigns stream->addr[0..(stream->size-1)];
+                assigns addr[0..(size-1)];
 
-                ensures GHI_bitpos: stream->bitpos == \old(stream->bitpos) + 17;
-                ensures GHI_size:   stream->size == \old(stream->size);
-                ensures GHI_left:   BitsUnchanged{Here,Old}(stream->addr, 0, pos + 11);
-                ensures GHI_equal:  BitstreamEqual64(stream, pos + 11, pos + 28,  p->GHI);
-                ensures GHI_right:  BitsUnchanged{Here,Old}(stream->addr, pos + 28, 8 * stream->size);
+                ensures GHI_left:   BitsUnchanged{Here,Old}(addr, 0, pos + 11);
+                ensures GHI_equal:  EqualBits64(addr, pos + 11, pos + 28,  p->GHI);
+                ensures GHI_right:  BitsUnchanged{Here,Old}(addr, pos + 28, 8 * size);
             */
-            Bitstream_Write_Normal(stream, 17, p->GHI);
+            Bitwalker_Poke_Normal(addr, size, pos + 11, 17, p->GHI);
+
+            stream->bitpos += 28;
 
             return 1;
         }
