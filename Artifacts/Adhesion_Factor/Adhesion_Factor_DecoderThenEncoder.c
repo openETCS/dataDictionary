7e1b1d12dbdefd5c3007758fa5e99d8bcf89ebb0
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2015-01-03 14:03:54 +0100

    update Adhesion_Factor_DecoderThenEncoder.c

--- Artifacts/Adhesion_Factor/Adhesion_Factor_DecoderThenEncoder.c
+++ Artifacts/Adhesion_Factor/Adhesion_Factor_DecoderThenEncoder.c
@@ -3,13 +3,15 @@
 #include "Adhesion_Factor_Encoder.h"
 
 /*@
-    requires valid_stream: \valid(stream);
-    requires stream_inv:   BitstreamInvariant(stream);
-    requires max_pos:      stream->bitpos + BitSize(p) <= UINT32_MAX;
-    requires max_pos:      NormalBitsequence(stream, BitSize(p));
+    requires valid:      Writeable(stream);
+
+    requires invariant:  Invariant(stream, BitSize(p));
+
+    requires normal:     Normal(stream, BitSize(p));
+
     requires \valid(p);
-    requires \separated(stream, p);
-    requires \separated(stream->addr + (0..stream->size-1), p);
+
+    requires Separated(stream, p);
 
     assigns stream->addr[0..stream->size-1];
     assigns stream->bitpos;
@@ -22,22 +24,22 @@ void Adhesion_Factor_DecoderThenEncoder(Bitstream* stream, Adhesion_Factor* p)
     const uint32_t length = ADHESION_FACTOR_BITSIZE;
     //@ ghost uint32_t pos = stream->bitpos;
 
-    //@ assert NormalBitsequence(stream, length);
+    //@ assert Normal(stream, length);
     Adhesion_Factor_Decoder(stream, p);
 
     //@ assert new_pos:    stream->bitpos == pos + length;
     //@ assert size:       stream->size   == \at(stream->size, Pre);
-    //@ assert copied1:    BitstreamEqual(stream, pos, p);
+    //@ assert copied:     BitstreamEqual(stream, pos, p);
     //@ assert not_set:    UpperBitsNotSet(p);
     //@ assert increment:  stream->bitpos == pos + length;
 
     stream->bitpos -= length;
     //@ assert stream->bitpos == pos;
-    //@ assert NormalBitsequence(stream, length);
+    //@ assert Normal(stream, length);
 
     Adhesion_Factor_Encoder(stream, p);
 
-    //@ assert unchanged_left:  BitstreamUnchanged{Here,Pre}(stream, 0, pos);
-    //@ assert copied2:         BitstreamEqual(stream, pos, p);
-    //@ assert unchanged_right: BitstreamUnchanged{Here,Pre}(stream, pos + length, 8 * stream->size);
+    //@ assert left:    BitstreamUnchanged{Here,Pre}(stream, 0, pos);
+    //@ assert middle:  BitstreamEqual(stream, pos, p);
+    //@ assert right:   BitstreamUnchanged{Here,Pre}(stream, pos + length, 8 * stream->size);
 }
