7d4d73128c827dd8972357b657654615f0ee69c8
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2015-01-04 10:51:55 +0100

    get rid of one coq proof for WriteTheRead

--- Artifacts/Bitwalker/Bitstream_WriteThenRead.c
+++ Artifacts/Bitwalker/Bitstream_WriteThenRead.c
@@ -14,14 +14,17 @@
     assigns stream->addr[0..stream->size-1];
     assigns stream->bitpos;
 
-    ensures unchanged:  value == \result;
+    ensures unchanged:  \result == value;
 */
 uint64_t Bitstream_WriteThenRead(Bitstream* stream, const uint32_t length, uint64_t value)
 {
-    //@ assert Normal(stream, length);
     //@ ghost uint32_t pos = stream->bitpos;
 
     Bitstream_Write(stream, length, value);
+
+    //@ assert left:   BitstreamUnchanged{Here,Pre}(stream, 0, pos);
+    //@ assert middle: BitstreamEqual64(stream, pos, pos + length, value);
+    //@ assert right:  BitstreamUnchanged{Here,Pre}(stream, pos + length, 8 * stream->size);
 
     stream->bitpos -= length;
     //@ assert stream->bitpos == pos;
@@ -29,9 +32,8 @@ uint64_t Bitstream_WriteThenRead(Bitstream* stream, const uint32_t length, uint6
 
     uint64_t result = Bitstream_Read(stream, length);
 
-    //@ assert lower_bits: EqualBitRange(value, result, 0, length);
-    //@ assert upper_bits: EqualBitRange(value, result, length, 64);
-    //@ assert equal_bits: EqualBitRange(value, result, 0, 64);
+    //@ assert copied:  BitstreamEqual64(stream, pos, pos + length, result);
+    //@ assert upper:   UpperBitsNotSet(result, length);
 
     return result;
 }
