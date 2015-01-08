8180d6dc8b7e5ae35f6ec50269ebb06044618cd9
Timon Lapawczyk <timon.lapawczyk@fokus.fraunhofer.de>
2015-01-08 14:35:40 +0100

    new format of adhesion_factor files to match the generated files

--- Source/Adhesion_Factor/Adhesion_Factor_Init.c
+++ Source/Adhesion_Factor/Adhesion_Factor_Init.c
@@ -3,12 +3,12 @@
 
 void Adhesion_Factor_Init(Adhesion_Factor* p)
 {
-    p->NID_PACKET = 0;
-    p->Q_DIR      = 0;
-    p->L_PACKET   = 0;
-    p->Q_SCALE    = 0;
-    p->D_ADHESION = 0;
-    p->L_ADHESION = 0;
-    p->M_ADHESION = 0;
+    p->NID_PACKET        = 0;
+    p->Q_DIR             = 0;
+    p->L_PACKET          = 0;
+    p->Q_SCALE           = 0;
+    p->D_ADHESION        = 0;
+    p->L_ADHESION        = 0;
+    p->M_ADHESION        = 0;
 }
 
