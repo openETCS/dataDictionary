19f33e0b4b02f9955f3ee816d7288b028de1a4f2
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2015-01-08 11:22:56 +0100

    make core bit functions inline

--- Source/Bitwalker/Bit64.h
+++ Source/Bitwalker/Bit64.h
@@ -11,7 +11,13 @@
 
    ensures set_bit: \result != 0 <==> LeftBit64(value, pos);
 */
-int PeekBit64(uint64_t value, uint32_t pos);
+static inline int PeekBit64(uint64_t value, uint32_t pos)
+{
+    uint64_t mask = ((uint64_t) 1) << (63u - pos);
+    uint64_t flag = value & mask;
+
+    return flag != 0u;
+}
 
 
 /*@
@@ -19,17 +25,18 @@ int PeekBit64(uint64_t value, uint32_t pos);
 
     assigns \nothing;
 
-    ensures left:    LeftEqualBitRange(\result, value, 0,  pos);
-
-    ensures set_bit: flag != 0  <==>  LeftBit64(\result, pos);
-
-    ensures right:   LeftEqualBitRange(\result, value, pos + 1,  64);
-
-    ensures upper:   \forall integer i; i >= 64 - pos ==>
+    ensures left:     LeftEqualBitRange(\result, value, 0,  pos);
+    ensures set_bit:  flag != 0  <==>  LeftBit64(\result, pos);
+    ensures right:    LeftEqualBitRange(\result, value, pos + 1,  64);
+    ensures upper:    \forall integer i; i >= 64 - pos ==>
                          (UpperBitsNotSet(value, i) ==> UpperBitsNotSet(\result, i));
-
 */
-uint64_t PokeBit64(uint64_t value, uint32_t pos, int flag);
+static inline uint64_t PokeBit64(uint64_t value, uint32_t pos, int flag)
+{
+    uint64_t mask = ((uint64_t) 1u) << (63 - pos);
+
+    return (flag == 0) ? (value & ~mask) : (value | mask);
+}
 
 
 /*@
@@ -39,7 +46,21 @@ uint64_t PokeBit64(uint64_t value, uint32_t pos, int flag);
 
     ensures  not_set: \result <==> UpperBitsNotSet(value, length);
 */
-int UpperBitsNotSet64(uint64_t value, uint32_t length);
+static inline int UpperBitsNotSet64(uint64_t value, uint32_t length)
+{
+    if (length == 64)
+    {
+        return 1;
+    }
+    else
+    {
+        const uint64_t MaxValue = ((uint64_t) 1) << length;
+
+        return (value >= MaxValue) ? 0 : 1;
+    }
+}
+
+
 
 
 #endif  // BIT64_H_INCLUDED
--- Source/Bitwalker/Bit8.h
+++ Source/Bitwalker/Bit8.h
@@ -1,18 +1,23 @@
 
-
 #ifndef BIT8_H_INCLUDED
 #define BIT8_H_INCLUDED
 
 #include "FramaCBits.h"
 
 /*@
-    requires pre: pos < 8;
+    requires pre:  pos < 8;
 
     assigns \nothing;
 
-    ensures set_bit: \result != 0 <==> LeftBit8(value, pos);
+    ensures pos:  \result != 0 <==> LeftBit8(value, pos);
 */
-int PeekBit8(uint8_t value, uint32_t pos);
+static inline int PeekBit8(uint8_t value, uint32_t pos)
+{
+    uint8_t mask = ((uint8_t) 1) << (7u - pos);
+    uint8_t flag = value & mask;
+
+    return flag != 0;
+}
 
 
 /*@
@@ -20,13 +25,16 @@ int PeekBit8(uint8_t value, uint32_t pos);
 
     assigns \nothing;
 
-    ensures left:     LeftEqualBitRange(\result, value, 0,  pos);
-
-    ensures set_bit:  LeftBit8(\result, pos) <==> (flag != 0);
-
-    ensures right:    LeftEqualBitRange(\result, value, pos + 1,  8);
+    ensures left:   LeftEqualBitRange(\result, value, 0,  pos);
+    ensures pos:    LeftBit8(\result, pos) <==> (flag != 0);
+    ensures right:  LeftEqualBitRange(\result, value, pos + 1,  8);
 */
-uint8_t PokeBit8(uint8_t value, uint32_t pos, int flag);
+static inline uint8_t PokeBit8(uint8_t value, uint32_t pos, int flag)
+{
+    uint8_t mask = ((uint8_t) 1) << (7u - pos);
+
+    return (flag == 0) ? (value & ~mask) : (value | mask);
+}
 
 #endif // BIT8_H_INCLUDED
 
