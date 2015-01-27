93964e4ee2f29c5a98aae5d98cdbcd42495709a3
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2015-01-27 03:56:39 -0800

    fix typo

--- Source/Bitwalker/UpperBitsNotSet.h
+++ Source/Bitwalker/UpperBitsNotSet.h
@@ -4,7 +4,7 @@
 
 #include "FramaCBits.h"
 
-// not an inline function in because it cannot verified automatically right now
+// not an inline function because it cannot verified automatically right now
 
 /*@
     requires pre: length <= 64;
