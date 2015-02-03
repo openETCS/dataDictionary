80df23b832bcf79e8fee414b120a69c0043b1d1b
Timon Lapawczyk <timon.lapawczyk@fokus.fraunhofer.de>
2015-02-03 13:37:42 +0100

    first idea for the L_PACKET test in the decoder branch

--- Source/Adhesion_Factor/opnETCS_DecoderBranch.c
+++ Source/Adhesion_Factor/opnETCS_DecoderBranch.c
@@ -9,6 +9,8 @@ int TrackToTrain_PackageDecoder(Bitstream* stream, oETCS_Union *oETCS)
     TrackToTrain_Header union_Header;
     // This call has a return value
     TrackToTrain_Decoder(stream, &union_Header);
+
+    int ret = 0;
     
     // Previous to a potential data packet read it has to be checked
     // whether the decoding of the header was correct. (via the return value)
@@ -21,7 +23,7 @@ int TrackToTrain_PackageDecoder(Bitstream* stream, oETCS_Union *oETCS)
 	    Adhesion_Factor_Data_Decoder(stream, &union_Data);
 	    // Write the header and the data packet into the oETCS_Union oETCS
 	    // Set the enums Namespace ...
-	    return // CODE FOR CORRECT READ (maybe the value of the decoder return
+	    ret =  // CODE FOR CORRECT READ (maybe the value of the decoder return
        }    
        default :
        {
@@ -32,9 +34,19 @@ int TrackToTrain_PackageDecoder(Bitstream* stream, oETCS_Union *oETCS)
 	    // (while reading the header the bitposition is updated automatically)
             stream->bitpos = stream->bitpos + union_Header->L_PACKET - BitSize(&union_Header);;
 	    // Return value that has not benn used for the decoders yet
-	    return // ERRORCODE
+	    ret =  // ERRORCODE
        }
     };
+
+    if (ret == 0) {
+       // Do the L_PACKET test here and return the errorcode for
+       // wrong L_PACKET values or return 0
+       return ret;
+    } else {
+       // There has been an even more critical error than a wrong L_PACKET value
+       // return the critical errorcode
+       return ret;
+    }
 };
 
 int TrainToTrack_PackageDecoder(void* Stream, int FirstBitPos,  oETCS_Union *oETCS )
@@ -42,6 +54,8 @@ int TrainToTrack_PackageDecoder(void* Stream, int FirstBitPos,  oETCS_Union *oET
     TrainToTrack_Header union_Header;
     // This call has a return value
     TrainToTrack_Header_Decoder(stream, &union_Header);
+
+    int ret = 0;
     
     // Previous to a potential data packet read it has to be checked
     // whether the decoding of the header was correct. (via the return value)
@@ -54,7 +68,7 @@ int TrainToTrack_PackageDecoder(void* Stream, int FirstBitPos,  oETCS_Union *oET
 	    Position_Report_Data_Decoder(stream, &union_Data);
 	    // Write the header and the data packet into the oETCS_Union oETCS
 	    // Set the enums Namespace ...
-	    return // CODE FOR CORRECT READ (maybe the value of the decoder return
+	    ret =  // CODE FOR CORRECT READ (maybe the value of the decoder return
        }    
        default :
        {
@@ -65,9 +79,19 @@ int TrainToTrack_PackageDecoder(void* Stream, int FirstBitPos,  oETCS_Union *oET
 	    // (while reading the header the bitposition is updated automatically)
             stream->bitpos = stream->bitpos + union_Header->L_PACKET - BitSize(&union_Header);;
 	    // Return value that has not benn used for the decoders yet
-	    return // ERRORCODE
+	    ret =  // ERRORCODE
        }
     };
+
+    if (ret == 0) {
+       // Do the L_PACKET test here and return the errorcode for
+       // wrong L_PACKET values or return 0
+       return ret;
+    } else {
+       // There has been an even more critical error than a wrong L_PACKET value
+       // return the critical errorcode
+       return ret;
+    }
 };
 
 int BothWays_PackageDecoder(void* Stream, int FirstBitPos,  oETCS_Union *oETCS )
@@ -75,6 +99,8 @@ int BothWays_PackageDecoder(void* Stream, int FirstBitPos,  oETCS_Union *oETCS )
     BothWays_Header union_Header;
     // This call has a return value
     BothWays_Header_Decoder(stream, &union_Header);
+
+    int ret = 0;
     
     // Previous to a potential data packet read it has to be checked
     // whether the decoding of the header was correct. (via the return value)
@@ -87,7 +113,7 @@ int BothWays_PackageDecoder(void* Stream, int FirstBitPos,  oETCS_Union *oETCS )
 	    End_of_Information_Data_Decoder(stream, &union_Data);
 	    // Write the header and the data packet into the oETCS_Union oETCS
 	    // Set the enums Namespace ...
-	    return // CODE FOR CORRECT READ (maybe the value of the decoder return
+	    ret =  // CODE FOR CORRECT READ (maybe the value of the decoder return
        }    
        default :
        {
@@ -98,8 +124,18 @@ int BothWays_PackageDecoder(void* Stream, int FirstBitPos,  oETCS_Union *oETCS )
 	    // (while reading the header the bitposition is updated automatically)
             stream->bitpos = stream->bitpos + union_Header->L_PACKET - BitSize(&union_Header);;
 	    // Return value that has not benn used for the decoders yet
-	    return // ERRORCODE
+	    ret =  // ERRORCODE
        }
     };
+
+    if (ret == 0) {
+       // Do the L_PACKET test here and return the errorcode for
+       // wrong L_PACKET values or return 0
+       return ret;
+    } else {
+       // There has been an even more critical error than a wrong L_PACKET value
+       // return the critical errorcode
+       return ret;
+    }
 };
 
