51ff91e3d0fd51837cc2b0f349e9af956b74b07a
Timon Lapawczyk <timon.lapawczyk@fokus.fraunhofer.de>
2015-03-03 16:54:06 +0100

    delete old Adhesion_Factor model directory. Everything in Drafts now

--- Source/Adhesion_Factor/BothWays/BothWays_Header_Init.c
+++
@@ -1,8 +0,0 @@
-
-#include "BothWays_Header_Init.h"
-
-void BothWays_Header_Init(BothWays_Header* p)
-{
-    p->NID_PACKET             = 0;
-}
-
--- Source/Adhesion_Factor/TrainToTrack_Header/TrainToTrack_Header_Init.c
+++
@@ -1,9 +0,0 @@
-
-#include "TrainToTrack_Header_Init.h"
-
-void TrainToTrack_Header_Init(TrainToTrack_Header* p)
-{
-    p->NID_PACKET             = 0;
-    p->L_PACKET               = 0;
-}
-
