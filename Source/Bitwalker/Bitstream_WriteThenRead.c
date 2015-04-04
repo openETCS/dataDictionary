89898ab751399c64bec922a6f6168f1b826fe45f
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2015-04-04 18:23:52 +0200

    add WriteThenRead

---
+++ Source/Bitwalker/Bitstream_WriteThenRead.c
@@ -0,0 +1,29 @@
+
+#include "Bitstream_Read.h"
+#include "Bitstream_Write.h"
+
+/*@
+    requires valid:      Writeable(stream);
+    requires invariant:  Invariant(stream, length);
+    requires normal:     Normal(stream, length);
+    requires upper:      UpperBitsNotSet(value, length);
+
+    assigns stream->addr[0..stream->size-1];
+    assigns stream->bitpos;
+
+    ensures result_value:  \result == value;
+*/
+uint64_t Bitstream_WriteThenRead(Bitstream* stream, uint32_t length, uint64_t value)
+{
+    Bitstream_Write(stream, length, value);
+
+    stream->bitpos -= length;
+
+    uint64_t result = Bitstream_Read(stream, length);
+
+    //@ assert partial_copy: EqualBitRange64(result, value, 0, length);
+    //@ assert full_copy:    EqualBitRange64(result, value, 0, 64);
+
+    return result;
+}
+
