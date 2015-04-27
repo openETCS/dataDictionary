7fb4bb6551dff8e2ee6472002828c03c252190db
Timon Lapawczyk <timon.lapawczyk@fokus.fraunhofer.de>
2015-04-27 16:26:15 +0200

    first logic functions for bitpos verification

--- Source/Drafts/Infill_location_reference/Infill_location_reference.h
+++ Source/Drafts/Infill_location_reference/Infill_location_reference.h
@@ -27,24 +27,52 @@ typedef struct Infill_location_reference Infill_location_reference;
 
     logic integer MaxBitSize{L}(Infill_location_reference* p) = BitSize(p);
 
+    logic integer Q_DIR_B{L}(Infill_location_reference* p) = 0;
+    logic integer Q_DIR_E{L}(Infill_location_reference* p) =
+    	Q_DIR_B{L}(p) + 2;
+
+    logic integer L_PACKET_B{L}(Infill_location_reference* p) =
+        Q_DIR_E{L}(p);
+    logic integer L_PACKET_E{L}(Infill_location_reference* p) =
+        L_PACKET_B{L}(p) + 13;
+
+    logic integer Q_NEWCOUNTRY_B{L}(Infill_location_reference* p) =
+        L_PACKET_E{L}(p);
+    logic integer Q_NEWCOUNTRY_E{L}(Infill_location_reference* p) =
+        Q_NEWCOUNTRY_B{L}(p) + 1;
+
+    logic integer NID_C_B{L}(Infill_location_reference* p) =
+        Q_NEWCOUNTRY_E{L}(p);
+    axiomatic NID_C_E_Axiomatic
+    {
+        logic integer NID_C_E{L}(Infill_location_reference* p)
+	    reads p->Q_NEWCOUNTRY;
+	
+	axiom NID_C_E_read{L}: \forall Infill_location_reference* p;
+	    p->Q_NEWCOUNTRY == 1 ==> NID_C_E{L}(p) == NID_C_B{L}(p) + 10;
+
+	axiom NID_C_E_skip{L}: \forall Infill_location_reference* p;
+	    p->Q_NEWCOUNTRY != 1 ==> NID_C_E{L}(p) == NID_C_B{L}(p);
+    }
+
+    logic integer NID_BG_B{L}(Infill_location_reference* p) =
+        NID_C_E{L}(p);
+    logic integer NID_BG_E{L}(Infill_location_reference* p) =
+        NID_BG_B{L}(p) + 14;
+
     predicate Separated(Bitstream* stream, Infill_location_reference* p) =
       \separated(stream, p) &&
       \separated(stream->addr + (0..stream->size-1), p);
 
-    predicate Invariant(Infill_location_reference* p) =
-      Invariant(p->Q_DIR)             &&
-      Invariant(p->L_PACKET)          &&
-      Invariant(p->Q_NEWCOUNTRY);
-
-    predicate ZeroInitialized(Infill_location_reference* p) =
+    predicate Invariant(Infill_location_reference* p) = Invariant(p->Q_DIR)             && Invariant(p->L_PACKET)          && Invariant(p->Q_NEWCOUNTRY); predicate ZeroInitialized(Infill_location_reference* p) =
       ZeroInitialized(p->Q_DIR)             &&
       ZeroInitialized(p->L_PACKET)          &&
       ZeroInitialized(p->Q_NEWCOUNTRY);
 
-    predicate EqualBits(Bitstream* stream, integer pos, Infill_location_reference* p) =
-      EqualBits(stream, pos,       pos + 2,   p->Q_DIR)             &&
-      EqualBits(stream, pos + 2,   pos + 15,  p->L_PACKET)          &&
-      EqualBits(stream, pos + 15,  pos + 16,  p->Q_NEWCOUNTRY);
+    predicate EqualBits{L}(Bitstream* stream, integer pos, Infill_location_reference* p) =
+      EqualBits(stream, pos + Q_DIR_B{L}(p), pos + Q_DIR_E{L}(p), p->Q_DIR) &&
+      EqualBits(stream, pos + L_PACKET_B{L}(p), pos + L_PACKET_E{L}(p), p->L_PACKET) &&
+      EqualBits(stream, pos + Q_NEWCOUNTRY_B{L}(p), pos + Q_NEWCOUNTRY_E{L}(p), p->Q_NEWCOUNTRY);
 
 
     predicate UpperBitsNotSet(Infill_location_reference* p) =
--- Source/Drafts/Infill_location_reference/Infill_location_reference_Decoder.c
+++ Source/Drafts/Infill_location_reference/Infill_location_reference_Decoder.c
@@ -14,28 +14,28 @@ int Infill_location_reference_Decoder(Bitstream* stream, Infill_location_referen
 	   requires Q_DIR: stream->bitpos == pos;
 	   assigns         stream->bitpos;
 	   assigns         p->Q_DIR;
-	   ensures  Q_DIR: stream->bitpos == pos + 2;
-	   ensures  Q_DIR: EqualBits(stream, pos, pos + 2, p->Q_DIR);
+	   ensures  Q_DIR: stream->bitpos == pos + Q_DIR_E{Here}(p);
+	   ensures  Q_DIR: EqualBits(stream, pos + Q_DIR_B{Here}(p), pos + Q_DIR_E{Here}(p), p->Q_DIR);
 	   ensures  Q_DIR: UpperBitsNotSet(p->Q_DIR, 2);
 	*/
 	{ p->Q_DIR		= Bitstream_Read(stream, 2); }
 
         /*@
-	   requires L_PACKET: stream->bitpos == pos + 2;
+	   requires L_PACKET: stream->bitpos == pos + L_PACKET_B{Here}(p);
 	   assigns            stream->bitpos;
 	   assigns            p->L_PACKET;
-	   ensures  L_PACKET: stream->bitpos == pos + 15;
-	   ensures  L_PACKET: EqualBits(stream, pos + 2, pos + 15, p->L_PACKET);
+	   ensures  L_PACKET: stream->bitpos == pos + L_PACKET_E{Here}(p);
+	   ensures  L_PACKET: EqualBits(stream, pos + L_PACKET_B{Here}(p), pos + L_PACKET_E{Here}(p), p->L_PACKET);
 	   ensures  L_PACKET: UpperBitsNotSet(p->L_PACKET, 13);
 	*/
         { p->L_PACKET		= Bitstream_Read(stream, 13); }
 
         /*@
-	   requires Q_NEWCOUNTRY: stream->bitpos == pos + 15;
+	   requires Q_NEWCOUNTRY: stream->bitpos == pos + Q_NEWCOUNTRY_B{Here}(p);
 	   assigns                stream->bitpos;
 	   assigns                p->Q_NEWCOUNTRY;
-	   ensures  Q_NEWCOUNTRY: stream->bitpos == pos + 16;
-	   ensures  Q_NEWCOUNTRY: EqualBits(stream, pos + 15, pos + 16, p->Q_NEWCOUNTRY);
+	   ensures  Q_NEWCOUNTRY: stream->bitpos == pos + Q_NEWCOUNTRY_E{Here}(p);
+	   ensures  Q_NEWCOUNTRY: EqualBits(stream, pos + Q_NEWCOUNTRY_B{Here}(p), pos + Q_NEWCOUNTRY_E{Here}(p), p->Q_NEWCOUNTRY);
 	   ensures  Q_NEWCOUNTRY: UpperBitsNotSet(p->Q_NEWCOUNTRY, 1);
 	*/
 	{ p->Q_NEWCOUNTRY		= Bitstream_Read(stream, 1); }
@@ -49,16 +49,16 @@ int Infill_location_reference_Decoder(Bitstream* stream, Infill_location_referen
 
 	//@ assert stream->bitpos <= \at(stream->bitpos, Pre) + INFILL_LOCATION_REFERENCE_BITSIZE;
 
-	//@ assert Q_DIR:            EqualBits(stream, pos, pos + 2, p->Q_DIR);
+	//@ assert Q_DIR:            EqualBits(stream, pos + Q_DIR_B{Here}(p), pos + Q_DIR_E{Here}(p), p->Q_DIR);
 	//@ assert Q_DIR:            UpperBitsNotSet(p->Q_DIR, 2);
 
-	//@ assert L_PACKET:         EqualBits(stream, pos + 2, pos + 15, p->L_PACKET);
+	//@ assert L_PACKET:         EqualBits(stream, pos + L_PACKET_B{Here}(p), pos + L_PACKET_E{Here}(p), p->L_PACKET);
 	//@ assert L_PACKET:         UpperBitsNotSet(p->L_PACKET, 13);
 
-	//@ assert Q_NEQCOUNTRY:     EqualBits(stream, pos + 15, pos + 15, p->Q_NEWCOUNTRY);
+	//@ assert Q_NEQCOUNTRY:     EqualBits(stream, pos + Q_NEWCOUNTRY_B{Here}(p), pos + Q_NEWCOUNTRY_E{Here}(p), p->Q_NEWCOUNTRY);
 	//@ assert Q_NEWCOUNTRY:     UpperBitsNotSet(p->Q_NEWCOUNTRY, 1);
 
-	//@ assert final: EqualBits(stream, pos, p);
+	//@ assert final: EqualBits{Here}(stream, pos, p);
 
         return 1;
     }
