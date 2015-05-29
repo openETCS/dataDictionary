77357b9ed4ce58d67bd49e8719e50ff85c4af354
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2015-05-29 10:27:37 +0200

    add astyle options --indent-switches and --indent-classes

--- Source/Eurobalise_Telegram/Decoder_Branch.cpp
+++ Source/Eurobalise_Telegram/Decoder_Branch.cpp
@@ -5,52 +5,52 @@ BasePacketPtr Decoder_Branch(Bitstream* stream, Packet_Header header)
 {
     switch (header.NID_PACKET)
     {
-    case 5 :
-    {
-        auto ret = std::make_shared<Train_running_number>();
-
-        if (Train_running_number_Decoder(stream, &(ret->core)) == 1)
+        case 5 :
         {
-            return ret;
+            auto ret = std::make_shared<Train_running_number>();
+
+            if (Train_running_number_Decoder(stream, &(ret->core)) == 1)
+            {
+                return ret;
+            }
+            else
+            {
+                return BasePacketPtr();
+            }
         }
-        else
+
+        case 71 :
+        {
+            auto ret = std::make_shared<Adhesion_Factor>();
+
+            if (Adhesion_Factor_Decoder(stream, &(ret->core)) == 1)
+            {
+                return ret;
+            }
+            else
+            {
+                return BasePacketPtr();
+            }
+        }
+
+        case 255 :
+        {
+            auto ret = std::make_shared<End_of_Information>();
+
+            if (End_of_Information_Decoder(stream, &(ret->core)) == 1)
+            {
+                return ret;
+            }
+            else
+            {
+                return BasePacketPtr();
+            }
+        }
+
+        default :
         {
             return BasePacketPtr();
         }
-    }
-
-    case 71 :
-    {
-        auto ret = std::make_shared<Adhesion_Factor>();
-
-        if (Adhesion_Factor_Decoder(stream, &(ret->core)) == 1)
-        {
-            return ret;
-        }
-        else
-        {
-            return BasePacketPtr();
-        }
-    }
-
-    case 255 :
-    {
-        auto ret = std::make_shared<End_of_Information>();
-
-        if (End_of_Information_Decoder(stream, &(ret->core)) == 1)
-        {
-            return ret;
-        }
-        else
-        {
-            return BasePacketPtr();
-        }
-    }
-
-    default :
-    {
-        return BasePacketPtr();
-    }
     };
 }
 
