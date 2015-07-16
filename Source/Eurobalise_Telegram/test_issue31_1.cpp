40b4a8e5ad38d993f16eb1dfc9afbb52589a4e37
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2015-07-16 15:27:20 +0200

    print from position 0

--- Source/Eurobalise_Telegram/test_issue31_1.cpp
+++ Source/Eurobalise_Telegram/test_issue31_1.cpp
@@ -23,7 +23,7 @@ int main ()
 
     std::vector<uint8_t> raw_stream(1000);
     Bitstream stream;
-    Bitstream_Init(&stream, &(raw_stream[0]), raw_stream.size(), 73);
+    Bitstream_Init(&stream, &(raw_stream[0]), raw_stream.size(), 0);
 
     uint32_t init_pos = stream.bitpos;
 
