949dc16b07ca983360bd968ebecd9e9c95fccfc8
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2015-08-11 20:02:45 +0200

    verification not clear

--- Source/Bitwalker/Bitstream_Read.c
+++ Source/Bitwalker/Bitstream_Read.c
@@ -1,10 +1,26 @@
 
 #include "Bitstream_Read.h"
-#include "Bitwalker_Peek_Normal.h"
+#include "Bit8Array.h"
+#include "Bit64.h"
 
 uint64_t Bitstream_Read(Bitstream* s, uint32_t length)
 {
-    uint64_t value = Bitwalker_Peek_Normal(s->addr, s->size, s->bitpos, length);
+    uint64_t value = 0;
+    const uint32_t bitpos = s->bitpos;
+
+    /*@
+      loop invariant  bound:  bitpos <= i <= bitpos + length;
+      loop invariant  equal:  EqualBits(s->addr, bitpos, i, value, length);
+      loop invariant  upper:  UpperBitsNotSet(value, length);
+
+      loop assigns i, value;
+      loop variant bitpos + length - i;
+    */
+    for (uint32_t i = bitpos; i < bitpos + length; ++i)
+    {
+        int flag = PeekBit8Array(s->addr, s->size, i);
+        value = PokeBit64(value, (64u - length) + (i - bitpos), flag);
+    }
 
     s->bitpos += length;
 
