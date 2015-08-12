e28b84c99b1b2d02f8c2347f8182ddc7454fa717
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2015-08-12 01:42:31 -0700

    re-establish old implemnetation of Bitstream_Read

--- Source/Bitwalker/Bitstream_Read.c
+++ Source/Bitwalker/Bitstream_Read.c
@@ -1,26 +1,10 @@
 
 #include "Bitstream_Read.h"
-#include "Bit8Array.h"
-#include "Bit64.h"
+#include "Bitwalker_Peek_Normal.h"
 
 uint64_t Bitstream_Read(Bitstream* s, uint32_t length)
 {
-    uint64_t value = 0;
-    const uint32_t bitpos = s->bitpos;
-
-    /*@
-      loop invariant  bound:  bitpos <= i <= bitpos + length;
-      loop invariant  equal:  EqualBits(s->addr, bitpos, i, value, length);
-      loop invariant  upper:  UpperBitsNotSet(value, length);
-
-      loop assigns i, value;
-      loop variant bitpos + length - i;
-    */
-    for (uint32_t i = bitpos; i < bitpos + length; ++i)
-    {
-        int flag = PeekBit8Array(s->addr, s->size, i);
-        value = PokeBit64(value, (64u - length) + (i - bitpos), flag);
-    }
+    uint64_t value =  Bitwalker_Peek_Normal(s->addr, s->size, s->bitpos, length);
 
     s->bitpos += length;
 
