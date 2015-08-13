095dc17f769177d183042d9a97a7b6b71eebfb5d
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2015-08-13 19:48:17 +0200

    remove old assertions

--- Source/Bitwalker/Bitwalker_Read.c
+++ Source/Bitwalker/Bitwalker_Read.c
@@ -18,9 +18,6 @@ uint64_t Bitwalker_Read(uint8_t* addr, uint32_t size, uint32_t bitpos, uint32_t 
     {
         int flag = TestBit8Array(addr, size, i);
         value = SetBit64(value, (i - bitpos) + (64u - length), flag);
-        //@ assert (i - bitpos) + (64u - length) == 64 - (bitpos + length) + i;
-        //@ assert Bit8Array(addr, i) <==> BitTest(value, bitpos + length - i - 1);
-
     }
 
     return value;
