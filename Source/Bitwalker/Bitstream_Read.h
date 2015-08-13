a542059c8877cdc16379ab076b6675a1be7971a8
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2015-08-13 01:50:06 -0700

    formatting

--- Source/Bitwalker/Bitstream_Read.h
+++ Source/Bitwalker/Bitstream_Read.h
@@ -9,13 +9,13 @@
   requires  invariant:  Invariant(stream, length);
   requires  normal:     Normal(stream, length);
 
-  assigns  stream->bitpos;
+  assigns   stream->bitpos;
 
-  ensures  pos:        stream->bitpos == \old(stream->bitpos) + length;
-  ensures  changed:    EqualBits(stream, \old(stream->bitpos), stream->bitpos, \result);
-  ensures  upper:      UpperBitsNotSet(\result, length);
-  ensures  size:       stream->size == \old(stream->size);
-  ensures  unchanged:  Unchanged{Here,Old}(stream, 0, 8 * stream->size);
+  ensures   pos:        stream->bitpos == \old(stream->bitpos) + length;
+  ensures   changed:    EqualBits(stream, \old(stream->bitpos), stream->bitpos, \result);
+  ensures   upper:      UpperBitsNotSet(\result, length);
+  ensures   size:       stream->size == \old(stream->size);
+  ensures   unchanged:  Unchanged{Here,Old}(stream, 0, 8 * stream->size);
 */
 uint64_t Bitstream_Read(Bitstream* stream, uint32_t length);
 
