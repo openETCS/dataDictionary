93cfa39d66611f649e5a1d32c5c45d72e3bb3a7a
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2015-08-13 06:59:53 -0700

    formart files

--- Source/Bitwalker/Bitstream_Normal.c
+++ Source/Bitwalker/Bitstream_Normal.c
@@ -3,6 +3,6 @@
 
 int Bitstream_Normal(const Bitstream* stream, uint32_t length)
 {
-    return stream->bitpos + length <= 8*stream->size;
+    return stream->bitpos + length <= 8 * stream->size;
 }
 
