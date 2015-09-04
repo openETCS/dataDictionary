d7fa1c4797b7b9d4b9d71bfebd5a5bf444fe3290
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2015-09-04 14:33:22 +0200

    merge simple.c and merge.c into all.c

--- Source/Foundation/Bits/all.c
+++ Source/Foundation/Bits/all.c
@@ -1,4 +1,12 @@
 
-#include "simple.c"
-#include "complex.c"
+#include "Bitwalker_Read.c"
+#include "Bitwalker_Write.c"
+#include "Bitstream_Init.c"
+#include "Bitstream_Normal.c"
+#include "Bitstream_Read.c"
+#include "Bitstream_Write.c"
+#include "UpperBitsNotSet.c"
+
+#include "Bitstream_WriteThenRead.c"
+#include "Bitstream_ReadThenWrite.c"
 
--- Source/Foundation/Bits/simple.c
+++
@@ -1,9 +0,0 @@
-
-#include "Bitwalker_Read.c"
-#include "Bitwalker_Write.c"
-#include "Bitstream_Init.c"
-#include "Bitstream_Normal.c"
-#include "Bitstream_Read.c"
-#include "Bitstream_Write.c"
-#include "UpperBitsNotSet.c"
-
