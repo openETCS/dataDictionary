d729514bd8f1b58e7083a2985968131e52291009
Marc Behrens <Marc.Behrens@DLR.de>
2015-10-08 12:45:25 +0200

    Delete StartEtcsSefev.sh
    cleanup

--- Source/Baseline3/Integration/StartEtcsSefev.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Starting SEFEV"
-/home/railsite/ITE/bin/run.sh -load /home/railsite/ITE/config/scenario-tsd-rca.xml -mdb /media/sf_D_DRIVE/OpenETCS_git/testmodel/AU_TS_AU_Testspec_150730_v290_01.mdb -lof /home/railsite/Sefev.log -autoload -autorun -autoexit
