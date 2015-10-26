1f4f73ee7c9a966d8f626d145ea602496ae2be1d
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2015-10-26 14:20:23 +0100

    remove more test output

--- Source2/Baseline2/Experiment/AdhesionFactor_Tests.c
+++ Source2/Baseline2/Experiment/AdhesionFactor_Tests.c
@@ -31,48 +31,48 @@ void AdhesionFactor_Tests(void)
 
     // new and delete
     {
-        printf("begin test AdhesionFactor (de)allocation \n");
+        printf("\tbegin test AdhesionFactor (de)allocation \n");
         AdhesionFactor* p = AdhesionFactor_New();
         AdhesionFactor_Init(p);
         //AdhesionFactor_Print(p, stdout); printf("\n");
-        printf("allocated at %p\n", p);
+        //printf("allocated at %p\n", p);
 
         AdhesionFactor* q = AdhesionFactor_New();
         AdhesionFactor_Init(q);
         //AdhesionFactor_Print(q, stdout); printf("\n");
-        printf("allocated at %p\n", q);
+        //printf("allocated at %p\n", q);
 
         AdhesionFactor* r = AdhesionFactor_New();
         AdhesionFactor_Init(r);
         //AdhesionFactor_Print(r, stdout); printf("\n");
-        printf("allocated at %p\n", r);
+        //printf("allocated at %p\n", r);
 
         AdhesionFactor* s = AdhesionFactor_New();
         AdhesionFactor_Init(s);
         //AdhesionFactor_Print(s, stdout); printf("\n");
-        printf("allocated at %p\n", s);
+        //printf("allocated at %p\n", s);
 
-        printf("delete at %p\n", q);
+        //printf("delete at %p\n", q);
         AdhesionFactor_Delete(q);
-        printf("delete at %p\n", s);
+        //printf("delete at %p\n", s);
         AdhesionFactor_Delete(s);
 
         AdhesionFactor* t = AdhesionFactor_New();
         AdhesionFactor_Init(t);
         //AdhesionFactor_Print(t, stdout); printf("\n");
-        printf("allocated at %p\n", t);
+        //printf("allocated at %p\n", t);
 
         AdhesionFactor* u = AdhesionFactor_New();
         AdhesionFactor_Init(u);
         //AdhesionFactor_Print(u, stdout); //printf("\n");
-        printf("allocated at %p\n", u);
+        //printf("allocated at %p\n", u);
 
         AdhesionFactor* v = AdhesionFactor_New();
         AdhesionFactor_Init(v);
         //AdhesionFactor_Print(v, stdout); printf("\n");
-        printf("allocated at %p\n", v);
+        //printf("allocated at %p\n", v);
 
-        printf("end test AdhesionFactor (de)allocation \n");
+        printf("\tend test AdhesionFactor (de)allocation \n");
     }
 }
 
