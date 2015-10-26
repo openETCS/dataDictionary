b282970eed63f1249d6b60d2f1c7dae6f4a5effb
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2015-10-26 14:21:46 +0100

    include (de)allocate tests from ../Experiment

--- Source2/Baseline2/Tests/AdhesionFactor_Tests.c
+++ Source2/Baseline2/Tests/AdhesionFactor_Tests.c
@@ -28,5 +28,51 @@ void AdhesionFactor_Tests(void)
     printf("\n");
     assert(AdhesionFactor_Equal(&a, p));
     AdhesionFactor_Delete(p);
+
+    // new and delete
+    {
+        printf("\tbegin test AdhesionFactor (de)allocation \n");
+        AdhesionFactor* p = AdhesionFactor_New();
+        AdhesionFactor_Init(p);
+        //AdhesionFactor_Print(p, stdout); printf("\n");
+        //printf("allocated at %p\n", p);
+
+        AdhesionFactor* q = AdhesionFactor_New();
+        AdhesionFactor_Init(q);
+        //AdhesionFactor_Print(q, stdout); printf("\n");
+        //printf("allocated at %p\n", q);
+
+        AdhesionFactor* r = AdhesionFactor_New();
+        AdhesionFactor_Init(r);
+        //AdhesionFactor_Print(r, stdout); printf("\n");
+        //printf("allocated at %p\n", r);
+
+        AdhesionFactor* s = AdhesionFactor_New();
+        AdhesionFactor_Init(s);
+        //AdhesionFactor_Print(s, stdout); printf("\n");
+        //printf("allocated at %p\n", s);
+
+        //printf("delete at %p\n", q);
+        AdhesionFactor_Delete(q);
+        //printf("delete at %p\n", s);
+        AdhesionFactor_Delete(s);
+
+        AdhesionFactor* t = AdhesionFactor_New();
+        AdhesionFactor_Init(t);
+        //AdhesionFactor_Print(t, stdout); printf("\n");
+        //printf("allocated at %p\n", t);
+
+        AdhesionFactor* u = AdhesionFactor_New();
+        AdhesionFactor_Init(u);
+        //AdhesionFactor_Print(u, stdout); //printf("\n");
+        //printf("allocated at %p\n", u);
+
+        AdhesionFactor* v = AdhesionFactor_New();
+        AdhesionFactor_Init(v);
+        //AdhesionFactor_Print(v, stdout); printf("\n");
+        //printf("allocated at %p\n", v);
+
+        printf("\tend test AdhesionFactor (de)allocation \n");
+    }
 }
 
